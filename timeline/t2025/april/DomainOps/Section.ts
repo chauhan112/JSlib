f8e990cebd8a587369562e6136aae91d87e0c855
raja <rajababuchauhan500@gmail.com>
2025-04-06 17:17:25 +0200

    refactored code which are commone and organized code related to domain ops

---
+++ timeline/t2025/april/DomainOps/Section.ts
@@ -0,0 +1,91 @@
+import { Tools } from "../GComponent";
+
+import {
+    create,
+    readAll,
+    deleteItem,
+    updateName,
+} from "../../mar/domainOps/Apis";
+import { GComponent } from "../GComponent";
+import { GForm } from "../GForm";
+import { ListWithCrud } from "../ListWithCrud";
+import { AccordionShowMany } from "../Accordion";
+export class Section {
+    form: any = null;
+    list: any = null;
+    typ: string = "";
+    content: GComponent | null = null;
+    plusIcon: GComponent | null = null;
+    funcs: any = null;
+    constructor(typ: string, form: GForm) {
+        this.typ = typ;
+        this.form = form;
+        this.list = new ListWithCrud();
+        this.list.getElement();
+        this.list.s.funcs.contextMenuClick = this.onContextMenuClick.bind(this);
+        this.content = this.makeContent();
+        this.funcs = {
+            updateName: (val: any) => val.domain,
+            createInfo: (val: any) => val.domain,
+            valuesForForm: (val: any) => {
+                return { domain: val.name };
+            },
+        };
+    }
+    makeContent() {
+        return Tools.div({
+            class: "w-full h-full",
+            children: [Tools.container({ key: "formArea" }), this.list],
+        });
+    }
+
+    fillList() {
+        readAll([], this.typ).then((res: any) => {
+            let domains = res.data.map((item: any) => {
+                return { name: item[1], key: item[0] };
+            });
+            this.list.setData(domains);
+        });
+    }
+    onEditSubmit(e: any, s: any) {
+        e.preventDefault();
+        if (s.values.domain !== this.form.s.currentData.name) {
+            updateName(
+                this.form.s.currentData.key,
+                [],
+                this.funcs.updateName(s.values),
+                this.typ
+            ).then((res: any) => {
+                this.fillList();
+                s.form.clearValues();
+                this.content!.s.formArea.clear();
+            });
+        }
+    }
+    onSubmitForCreate(e: any, s: any) {
+        e.preventDefault();
+        create(this.funcs.createInfo(s.values), [], this.typ).then(
+            (res: any) => {
+                console.log(this.plusIcon);
+                this.plusIcon!.component.dispatchEvent(new Event("click"));
+                s.form.clearValues();
+                this.fillList();
+            }
+        );
+    }
+
+    onContextMenuClick(e: any, s: any) {
+        if (s.item.name === "Delete") {
+            if (confirm("Are you sure you want to delete this domain?")) {
+                deleteItem(s.data.key, [], this.typ).then((res: any) => {
+                    this.fillList();
+                });
+            }
+        } else if (s.item.name === "Edit") {
+            this.content!.s.formArea.display(this.form);
+            this.form.s.funcs.onSubmit = this.onEditSubmit.bind(this);
+            this.form.s.currentData = s.data;
+            this.form.setValues(this.funcs.valuesForForm(s.data));
+        }
+    }
+}
---
+++ timeline/t2025/april/DomainOps/Sidebar.ts
@@ -0,0 +1,131 @@
+import { Tools } from "../GComponent";
+import { AccordionShowMany } from "../Accordion";
+import { Section } from "./Section";
+import { GForm, gformTest } from "../GForm";
+
+const accordion = new AccordionShowMany();
+
+accordion.s.funcs.onPlusHandlerOnShow = (e: any, s: any) => {
+    console.log(s);
+    if (s.item.title === "Domains") {
+        domainSection.content!.s.formArea.display(domainSection.form);
+        domainSection.form.s.funcs.onSubmit =
+            domainSection.onSubmitForCreate.bind(domainSection);
+        domainSection.form.clearValues();
+        domainSection.plusIcon = s.s;
+    } else if (s.item.title === "Operations") {
+        operationsSection.content!.s.formArea.display(operationsForm);
+        operationsSection.form.s.funcs.onSubmit =
+            operationsSection.onSubmitForCreate.bind(operationsSection);
+        operationsSection.form.clearValues();
+        operationsSection.plusIcon = s.s;
+    } else if (s.item.title === "Activities") {
+        activitiesSection.content!.s.formArea.display(activitiesForm);
+    }
+};
+accordion.s.funcs.onPlusHandlerOnHide = (e: any, s: any) => {
+    if (s.item.title === "Domains") {
+        domainSection.content!.s.formArea.clear();
+    } else if (s.item.title === "Operations") {
+        operationsSection.content!.s.formArea.clear();
+    } else if (s.item.title === "Activities") {
+        activitiesSection.content!.s.formArea.clear();
+    }
+};
+accordion.getElement();
+
+const makeDomainForm = () => {
+    const form = new GForm();
+    form.s.data[0].props.placeholder = "domain name";
+    form.s.data[0].props.class = "w-full p-1 rounded-sm bg-gray-100 text-black";
+    form.s.data[1].props.class = "w-full p-1 rounded-md bg-blue-500 text-white";
+    form.getElement();
+    return form;
+};
+
+const makeOperationsForm = () => {
+    const form = new GForm();
+    form.s.data[0].props.placeholder = "operation name";
+    form.s.data[0].props.key = "operation";
+    form.s.data[0].props.class = "w-full p-1 rounded-sm bg-gray-100 text-black";
+    form.s.data[1].props.class = "w-full p-1 rounded-md bg-blue-500 text-white";
+    form.getElement();
+    return form;
+};
+
+const makeActivitiesForm = () => {
+    const form = new GForm();
+    form.s.data = [
+        {
+            type: "select",
+            props: {
+                key: "domain",
+                class: "w-full p-1 rounded-sm bg-gray-100 text-black",
+            },
+        },
+        {
+            type: "select",
+            props: {
+                key: "operations",
+                placeholder: "Enter operations",
+                class: "w-full p-1 rounded-sm bg-gray-100 text-black",
+            },
+        },
+        {
+            type: "input",
+            props: {
+                type: "submit",
+                textContent: "Submit",
+                class: "w-full p-1 rounded-md bg-blue-500 text-white",
+            },
+        },
+    ];
+    form.getElement();
+    return form;
+};
+const operationsForm = makeOperationsForm();
+const activitiesForm = makeActivitiesForm();
+
+const domainSection = new Section("domains", makeDomainForm());
+const operationsSection = new Section("operations", operationsForm);
+const activitiesSection = new Section("logger", activitiesForm);
+
+domainSection.fillList();
+operationsSection.fillList();
+operationsSection.funcs = {
+    updateName: (val: any) => val.operation,
+    createInfo: (val: any) => val.operation,
+    valuesForForm: (val: any) => {
+        return { operation: val.name };
+    },
+};
+activitiesSection.fillList();
+
+accordion.setData([
+    {
+        title: "Domains",
+        content: domainSection.content,
+        open: true,
+    },
+    {
+        title: "Operations",
+        content: operationsSection.content,
+        open: true,
+    },
+    {
+        title: "Activities",
+        content: activitiesSection.content,
+        open: false,
+    },
+]);
+
+export const sidebar = Tools.div({
+    class: "w-64 bg-gray-700 text-white p-4 min-h-[100vh] flex flex-col gap-5 overflow-y-auto",
+    children: [
+        Tools.comp("h1", {
+            class: "text-lg font-bold",
+            textContent: "DOMAIN LOGGER",
+        }),
+        accordion,
+    ],
+});
