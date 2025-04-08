d6dc7f71be1840ae3fdcf3144496d393e845f253
raja <rajababuchauhan500@gmail.com>
2025-04-09 00:12:16 +0200

    made the list clickable using this wrapper

---
+++ timeline/t2025/april/DomainOps/ListWithCrudWrapper.ts
@@ -0,0 +1,56 @@
+import { ListWithCrud } from "../ListWithCrud";
+import { Tools } from "../tools";
+
+export class ListWithCrudWrapper {
+    list: ListWithCrud;
+    typ: string;
+    s: { [key: string]: any } = {};
+    constructor(typ: string, root: any) {
+        this.typ = typ;
+        this.list = new ListWithCrud();
+        this.list.s.funcs.createItem = this.createItem.bind(this);
+        this.s.funcs = {
+            itemClick: this.itemClick.bind(this),
+        };
+        this.s.root = root;
+    }
+    itemClick(e: any, ls: any) {
+        this.s.root.s.comps.content.clear();
+        this.s.root.s.comps.content.display(
+            Tools.comp("H1", {
+                textContent: ls.s.data.name + " " + this.typ,
+            })
+        );
+        this.s.root.propertySection.s.currentInfo = {
+            data: ls.s.data,
+            typ: this.typ,
+        };
+        this.s.root.propertySection.fetchProperties();
+        this.s.root.propertySection.show();
+        console.log(ls.s.data, this.typ);
+    }
+    createItem(item: any) {
+        return Tools.div({
+            class: "w-full flex items-center justify-between ",
+            children: [
+                Tools.comp(
+                    "button",
+                    {
+                        textContent: item.name,
+                        class: "cursor-pointer flex-1 rounded-md text-left",
+                    },
+                    {
+                        click: (e: any, ls: any) => {
+                            this.s.funcs.itemClick(e, ls);
+                        },
+                    },
+                    { data: item }
+                ),
+                Tools.div({
+                    class: "w-fit flex items-center justify-between",
+                    children: this.list.s.funcs.operationsMaker(item),
+                }),
+            ],
+        });
+    }
+}
