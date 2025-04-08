f05bddc27d14bde17cad60b6a4de2bff35590d59
raja <rajababuchauhan500@gmail.com>
2025-04-08 13:32:33 +0200

    adjusted code (CRUD) for logger because it was throwing error

--- timeline/t2025/mar/domainOps/Apis.js
+++ timeline/t2025/mar/domainOps/Apis.js
@@ -4,9 +4,21 @@ export const BASE_URL = "http://localhost:8000";
 export const create = async (name, loc, typ = "domains") => {
     return axios.post(`${BASE_URL}/${typ}/create/`, { name, loc });
 };
+export const createLogger = async (name, loc, domains, operation) => {
+    return axios.post(`${BASE_URL}/logger/create/`, {
+        name,
+        loc,
+        domains,
+        operation,
+    });
+};
 
 export const readAll = async (loc, typ = "domains") => {
     return axios.post(`${BASE_URL}/${typ}/readAll/`, { loc });
+};
+
+export const read = async (name, loc, typ = "domains") => {
+    return axios.post(`${BASE_URL}/${typ}/read/`, { name, loc });
 };
 
 export const deleteItem = async (name, loc, typ = "domains") => {
@@ -20,6 +32,15 @@ export const updateName = async (name, loc, newName, typ = "domains") => {
         value: newName,
     });
 };
+
+export const updateLogger = async (name, loc, vals) => {
+    return axios.post(`${BASE_URL}/loggers/update/`, {
+        name,
+        loc,
+        ...vals,
+    });
+};
+
 export const readProps = async (name, loc, typ = "domains") => {
     return axios.post(`${BASE_URL}/${typ}/readAll/`, {
         name,
