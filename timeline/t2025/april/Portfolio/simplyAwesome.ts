7389ac4c28a9bc1ea4dbcab751022b6ffe3c2ac9
raja <rajababuchauhan500@gmail.com>
2025-04-14 22:17:56 +0200

    refactored fancy title,card comp, implemnted row, btn, navigation, project and contact section

--- timeline/t2025/april/Portfolio/simplyAwesome.ts
+++ timeline/t2025/april/Portfolio/simplyAwesome.ts
@@ -8,8 +8,11 @@ import {
     FileText,
     Github,
     Linkedin,
+    Pi,
 } from "lucide";
 import "./style1.css";
+import { Undoers } from "../Array";
+import axios from "axios";
 
 export class RippleLines implements IComponent {
     s: { [key: string]: any } = {};
@@ -148,40 +151,10 @@ export class StyleComp implements IComponent {
     }
 }
 
-export class FancyTitle implements IComponent {
-    s: { [key: string]: any } = {};
-    comp: GComponent | null = null;
-    constructor(content: string = "Hello World") {
-        this.s.content = content;
-    }
-    getProps(): { [key: string]: any } {
-        return this.comp!.getProps();
-    }
-    getElement(): HTMLElement | SVGElement {
-        if (this.comp) {
-            return this.comp.getElement();
-        }
-        const c = Tools.div({
-            class: "relative mt-4",
-            children: [
-                Tools.div({
-                    class: "flex font-bold",
-                    textContent: "// " + this.s.content,
-                }),
-                Tools.div({
-                    class: "absolute bottom-0 left-0 opacity-15 text-4xl font-bold titleBack232342526",
-                    textContent: this.s.content,
-                }),
-            ],
-        });
-        this.comp = c;
-        return c.getElement();
-    }
-}
-
 export class Page implements IComponent {
     s: { [key: string]: any } = {};
     comp: GComponent | null = null;
+    undoer: Undoers = new Undoers();
     constructor() {
         this.s.comps = {};
         this.s.infos = {
@@ -278,7 +251,7 @@ export class Page implements IComponent {
         this.s.comps.typing.getElement();
 
         const c = Tools.div({
-            class: "w-full h-full flex flex-col items-center justify-center",
+            class: "w-full h-full flex flex-col items-center justify-center pb-8",
             children: [this.s.comps.ripple, this.header(), this.getMainPage()],
         });
 
@@ -313,35 +286,66 @@ export class Page implements IComponent {
         return Tools.comp("ul", {
             class: "flex list-none items-start text-2xl font-mono flex-col sticky top-0 w-4/12 w-min",
             children: this.s.infos.sections.map((val: string) => {
-                return Tools.comp("li", {
-                    class: "hover:bg-gray-100/50 p-1 rounded-md bg-white/80 w-full",
-                    child: Tools.comp("button", {
-                        class: "cursor-pointer w-full text-left",
-                        textContent: val,
-                    }),
-                });
+                return Tools.comp(
+                    "li",
+                    {
+                        class: " cursor-pointer flex hover:bg-gray-100/50 p-1 rounded-md bg-white/80 w-full",
+                        children: [
+                            Tools.comp("button", {
+                                class: "cursor-pointer w-full text-left",
+                                textContent: val,
+                            }),
+                            Tools.comp("div", {
+                                key: "arrow",
+                                class: "flex w-[30px] h-[30px] transparent rounded-full flex-shrink-0 ml-4 border border-dashed items-center justify-center",
+                            }),
+                        ],
+                    },
+                    {
+                        click: (e: any, ls: any) => {
+                            this.undoer.undo();
+                            let cc = this.s.comps[ls.s.data];
+                            cc.component.scrollIntoView({ behavior: "smooth" });
+                            ls.s.arrow.update({
+                                textContent: ">",
+                            });
+                            this.undoer.add(() => {
+                                ls.s.arrow.update({
+                                    textContent: "",
+                                });
+                            });
+                        },
+                    },
+                    {
+                        data: val,
+                    }
+                );
             }),
         });
     }
     getContent() {
+        this.s.comps = {
+            ...this.s.comps,
+            "About Me": this.intro(),
+            Resume: this.educationAndExperience(),
+            Portfolio: this.works(),
+            Contact: this.contact(),
+        };
         return Tools.div({
             class: "w-full h-full flex flex-col gap-4 ",
             children: [
-                this.intro(),
-                this.educationAndExperience(),
-                this.works(),
-                this.contact(),
+                this.s.comps["About Me"],
+                this.s.comps.Resume,
+                this.s.comps.Portfolio,
+                this.s.comps.Contact,
             ],
         });
     }
     intro() {
-        let FancyTitleComp = new FancyTitle("About Me");
-        FancyTitleComp.getElement();
         this.s.comps.typing.comp.update({
             class: "text-xl font-mono absolute bottom-2 left-4 bg-gray-100/50 ",
         });
-        return Tools.div({
-            class: "flex flex-col gap-8 p-8 rounded-md shadow-[0_8px_26px_0_rgba(22,24,26,0.07)] hover:shadow-[0_8px_32px_0_rgba(22,24,26,0.11)] bg-white/80",
+        return CardComp({
             children: [
                 Tools.div({
                     class: "flex flex-col flex-1 gap-8 mt-4 md:flex-row",
@@ -359,7 +363,7 @@ export class Page implements IComponent {
                         Tools.div({
                             class: "flex flex-col h-full",
                             children: [
-                                FancyTitleComp,
+                                FancyTitle("About Me"),
                                 Tools.div({
                                     class: "text-4xl font-mono mt-4 font-bold ",
                                     textContent: "AI & Software Engineer",
@@ -405,62 +409,132 @@ export class Page implements IComponent {
         });
     }
     educationAndExperience() {
-        let FancyTitleComp = new FancyTitle("Resume");
-        FancyTitleComp.getElement();
-        return Tools.div({
-            class: "flex flex-col gap-4 p-4 rounded-md shadow-[0_8px_26px_0_rgba(22,24,26,0.07)] hover:shadow-[0_8px_32px_0_rgba(22,24,26,0.11)] bg-white/80",
+        return CardComp({
             children: [
-                FancyTitleComp,
+                FancyTitle("Resume"),
                 Tools.div({
                     class: "text-4xl font-mono font-bold ",
                     textContent: "Education & Experience",
                 }),
                 Tools.div({
-                    class: "flex flex-col gap-4 mt-4 wrap sm:flex-row",
+                    class: "flex gap-4 mt-4 flex-wrap ",
                     children: [
-                        timeline(this.s.infos.educations, GraduationCap),
-                        timeline(this.s.infos.experiences, BriefcaseBusiness),
+                        TimelineComp(this.s.infos.educations, GraduationCap),
+                        TimelineComp(
+                            this.s.infos.experiences,
+                            BriefcaseBusiness
+                        ),
                     ],
                 }),
             ],
         });
     }
     works() {
-        let FancyTitleComp = new FancyTitle("Portfolio");
-        FancyTitleComp.getElement();
-        return Tools.div({
-            class: "flex flex-col gap-4 rounded-md shadow-[0_8px_26px_0_rgba(22,24,26,0.07)] hover:shadow-[0_8px_32px_0_rgba(22,24,26,0.11)] bg-white/80",
+        return CardComp({
             children: [
-                FancyTitleComp,
+                FancyTitle("Portfolio"),
                 Tools.div({
                     class: "text-4xl font-mono font-bold ",
                     textContent: "My Latest Works",
                 }),
                 Tools.div({
-                    children: [],
+                    class: "flex flex-wrap justify-center ",
+                    children: this.s.infos.projects.map((x: any) => {
+                        return Tools.comp("a", {
+                            class: "relative w-[400px] p-4 rounded-md bg-gray-100/80 h-[200px] mt-2 ml-2 cursor-pointer hover:bg-gray-200/80",
+                            children: [
+                                Tools.comp("p", {
+                                    class: "absolute flex text-sm justify-end top-2 right-2",
+                                    textContent: x.tech,
+                                }),
+                                Tools.comp("div", {
+                                    class: "flex text-2xl font-bold items-center justify-center h-full",
+                                    textContent: x.title,
+                                }),
+                            ],
+                            href: x.link,
+                        });
+                    }),
                 }),
             ],
         });
     }
     contact() {
-        let FancyTitleComp = new FancyTitle("Contact Me");
-        FancyTitleComp.getElement();
-        return Tools.div({
+        let contactStatus = Tools.div({});
+        return CardComp({
             children: [
-                FancyTitleComp,
+                FancyTitle("Contact Me"),
                 Tools.comp("ul", {
-                    class: "list-disc",
+                    class: "flex gap-4 list-none ",
                     children: [
-                        Tools.comp("li", {
-                            textContent:
-                                "Bachelor of Science in Computer Science",
-                        }),
-                        Tools.comp("li", {
-                            textContent:
-                                "Software Engineer at XYZ Company (2020 - Present)",
-                        }),
+                        PillComp("Email: rajababuchauhan500@gmail.com"),
+                        PillComp("Phone: +49 162 7078024"),
                     ],
                 }),
+                Tools.comp(
+                    "form",
+                    {
+                        class: "flex flex-col gap-4 mt-4 ",
+                        children: [
+                            Row(
+                                Tools.comp("input", {
+                                    class: "border-b border-dashed border-black/20  p-2 flex-1 focus:outline-none",
+                                    placeholder: "Name",
+                                    name: "name",
+                                    required: true,
+                                }),
+                                Tools.comp("input", {
+                                    class: "border-b border-dashed border-black/20  p-2 flex-1 focus:outline-none",
+                                    placeholder: "E-mail",
+                                    type: "email",
+                                    name: "email",
+                                    required: true,
+                                })
+                            ),
+                            Tools.comp("input", {
+                                class: "border-b border-dashed border-black/20  p-2 flex-1 focus:outline-none",
+                                placeholder: "Subject",
+                                name: "subject",
+                                required: true,
+                            }),
+                            Tools.comp("textarea", {
+                                class: "focus:outline-none border-b border-dashed border-black/20 resize-y p-2 h-[142px]",
+                                placeholder: "Message",
+                                name: "message",
+                                required: true,
+                            }),
+                            Button(),
+                            contactStatus,
+                        ],
+                        action: "https://formspree.io/f/xwplzpyv",
+                        method: "POST",
+                    },
+                    {
+                        submit: (e: any, ls: any) => {
+                            e.preventDefault();
+                            let fd = new FormData(e.target);
+                            axios
+                                .post(e.target.action, fd)
+                                .then((res: any) => {
+                                    contactStatus.update({
+                                        textContent: `Thanks for your message! We will get back to you soon.`,
+                                        class: "mt-4 p-3 rounded text-sm bg-green-100 border border-green-400 text-green-700",
+                                    });
+                                    e.target.reset();
+                                })
+                                .catch((err: any) => {
+                                    console.error(
+                                        "Form submission error:",
+                                        err
+                                    );
+                                    contactStatus.update({
+                                        textContent: `Oops! There was a problem submitting your form: ${err.message}`,
+                                        class: "mt-4 p-3 rounded text-sm  bg-red-100 border border-red-400 text-red-700",
+                                    });
+                                });
+                        },
+                    }
+                ),
             ],
         });
     }
@@ -480,7 +554,7 @@ export const PillCompExp = (x: string) => {
     });
 };
 
-export const timeline = (items: any[], icon: IconNode) => {
+export const TimelineComp = (items: any[], icon: IconNode) => {
     return Tools.div({
         class: "flex-1 pl-6 border-l-2 border-dashed border-black/20",
         children: [
@@ -502,5 +576,44 @@ export const timeline = (items: any[], icon: IconNode) => {
                 });
             }),
         ],
+    });
+};
+
+export const CardComp = (x: any) => {
+    return Tools.div({
+        class: "flex flex-col gap-8 p-8 rounded-md shadow-[0_8px_26px_0_rgba(22,24,26,0.07)] hover:shadow-[0_8px_32px_0_rgba(22,24,26,0.11)] bg-white/80",
+        ...x,
+    });
+};
+
+export const FancyTitle = (title: string, props: any = {}) => {
+    return Tools.div({
+        class: "relative mt-4",
+        children: [
+            Tools.div({
+                class: "flex font-bold",
+                textContent: "// " + title,
+            }),
+            Tools.div({
+                class: "absolute bottom-0 left-0 opacity-15 text-4xl font-bold titleBack232342526",
+                textContent: title,
+            }),
+        ],
+        ...props,
+    });
+};
+
+export const Row = (...x: any[]) => {
+    return Tools.div({
+        class: "flex gap-4",
+        children: x,
+    });
+};
+
+export const Button = () => {
+    return Tools.comp("button", {
+        class: "border border-dashed border-black/20 rounded-md p-2 w-fit cursor-pointer hover:bg-black/10",
+        textContent: "Send Message",
+        type: "submit",
     });
 };
