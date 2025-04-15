a812123f62185ce1c5f65688cbc061861e9c8cd0
raja <rajababuchauhan500@gmail.com>
2025-04-15 22:52:49 +0200

    created unstructured form

--- timeline/t2025/april/DomainOps/Home.ts
+++ timeline/t2025/april/DomainOps/Home.ts
@@ -1,9 +1,19 @@
-import { DocumentHandler } from "../Array";
 import { GComponent, IComponent } from "../GComponent";
 import { Tools } from "../tools";
 import { Sidebar } from "./Sidebar";
 import { PropertySection } from "./PropertySection";
-import { Search, Menu, Eye, Pencil, Trash, Plus } from "lucide";
+import { Main as ModalMain } from "../Modal";
+import {
+    Search,
+    Menu,
+    Eye,
+    Pencil,
+    Trash,
+    Plus,
+    Clock,
+    Pen,
+    IconNode,
+} from "lucide";
 
 export class Home implements IComponent {
     s: { [key: string]: any } = {};
@@ -21,7 +31,7 @@ export class Home implements IComponent {
             return this.comp.getElement();
         }
         this.s.comps = {};
-        this.s.comps.content = MainBody();
+        this.s.comps.content = MainBody(this);
         this.comp = Tools.div({
             class: "w-full h-full flex justify-between",
             children: [
@@ -37,16 +47,17 @@ export class Home implements IComponent {
         return this.comp!.getProps();
     }
 }
-
-export const MainBody = () => {
+export const MainBody = (root?: any) => {
     return Tools.container({
+        key: "mainBody",
         class: "flex flex-col flex-1",
-        children: [Header(), Table(), Footer()],
+        children: [Header(root), Table(root), Footer(root)],
     });
 };
-export const Header = () => {
+export const Header = (root?: any) => {
     return Tools.div({
         class: "bg-[#F5C85F] px-6 py-3 shadow",
+        key: "header",
         children: [
             Tools.div({
                 class: "flex justify-between items-center",
@@ -89,18 +100,34 @@ export const Header = () => {
         ],
     });
 };
-export const Footer = () => {
+export const Footer = (root?: any) => {
+    let uform = FormUnstructured();
+    let modal = ModalMain.modal();
+
+    uform.s.form.s.cancelSave.s.cancel.update(
+        {},
+        { click: () => modal.hide() }
+    );
     return Tools.div({
         class: " flex justify-end",
         children: [
+            modal,
             Tools.comp("button", {
                 class: "cursor-pointer mb-4 mr-4 bg-[#F5C85F] hover:bg-[#eab330] text-gray-800 rounded-full w-10 h-10 flex items-center justify-center text-xl font-bold shadow-md",
-                child: Tools.icon(Plus),
+                child: Tools.icon(
+                    Plus,
+                    {},
+                    {
+                        click: (e: any, ls: any) => {
+                            modal.display(uform);
+                        },
+                    }
+                ),
             }),
         ],
     });
 };
-export const Table = () => {
+export const Table = (root?: any) => {
     return Tools.div({
         class: "flex flex-col flex-1 overflow-y-auto px-4",
         children: [
@@ -154,3 +181,85 @@ export const Row = (nr: any) => {
         ],
     });
 };
+export const FormUnstructured = () => {
+    const DateInfoComp = (icon: IconNode, title: string, val?: string) => {
+        return Tools.div({
+            class: "flex gap-3 mb-3",
+            children: [
+                Tools.icon(icon, {
+                    class: "w-5",
+                }),
+                Tools.div({
+                    class: "font-semibold",
+                    textContent: title,
+                }),
+                Tools.comp("input", {
+                    class: "ms-5 px-2 outline-teal-300 rounded bg-teal-100",
+                    type: "text",
+                    disabled: true,
+                    value: val
+                        ? new Date(parseInt(val)).toLocaleString()
+                        : new Date().toLocaleString(),
+                }),
+            ],
+        });
+    };
+    const TextArea = (props: Record<string, any> = {}) => {
+        return Tools.comp("textarea", {
+            class: "mb-3 outline-slate-500 px-3 py-2 w-full rounded-xl flex-1 bg-white",
+            ...props,
+        });
+    };
+    return Tools.div({
+        class: "flex flex-col h-dvh bg-slate-500",
+        children: [
+            Tools.div({
+                class: "bg-teal-200",
+                children: [
+                    Tools.div({
+                        class: "py-5 px-7",
+                        children: [
+                            Tools.comp("input", {
+                                class: "outline-teal-500 text-3xl mb-3 font-bold bg-teal-200 border pb-1 px-2",
+                                children: [],
+                                placeholder: "Activity_name",
+                                type: "text",
+                                name: "title",
+                            }),
+                            DateInfoComp(Clock, "Created_on"),
+                            DateInfoComp(Pen, "Modified_on"),
+                        ],
+                    }),
+                ],
+            }),
+            Tools.div({
+                key: "form",
+                class: "flex flex-col flex-1 p-5",
+                children: [
+                    TextArea({ placeholder: "Did", name: "did" }),
+                    TextArea({ placeholder: "Next", name: "next" }),
+                    CancelSaveBtns(),
+                ],
+            }),
+        ],
+    });
+};
+export const CancelSaveBtns = () => {
+    return Tools.div({
+        key: "cancelSave",
+        class: "flex gap-5 justify-end",
+        children: [
+            Tools.comp("button", {
+                key: "cancel",
+                class: "bg-red-500 w-fit px-10 py-1 font-bold rounded-xl cursor-pointer on hover:bg-red-600",
+                textContent: "Cancel",
+            }),
+            Tools.comp("button", {
+                key: "save",
+                class: "bg-green-500 w-fit px-10 py-1 font-bold rounded-xl cursor-pointer on hover:bg-green-600",
+                textContent: "Save",
+            }),
+        ],
+    });
+};
+export const StructuredForm = (struct: any) => {};
