d876a3251fdc71d88b451af59b6f0ef960cb6eaf
raja <rajababuchauhan500@gmail.com>
2025-05-07 22:32:14 +0200

    Components for header

---
+++ timeline/t2025/may/ToolsHomepage/Components.ts
@@ -0,0 +1,40 @@
+import { Tools } from "../../april/tools";
+import { Home } from "lucide";
+// import { logo } from "./logo.png";
+
+export const Logo = () => {
+    return Tools.div({
+        class: "text-center flex-grow",
+        children: [
+            Tools.comp("h1", {
+                class: "text-4xl font-bold  mb-2",
+                textContent: "RajaDevKit",
+            }),
+        ],
+    });
+};
+
+export const MainBody = () => {
+    return Tools.comp("main", {
+        class: "container mx-auto p-4",
+        children: [
+            Tools.comp("p", {
+                textContent: "Page content goes here...",
+            }),
+        ],
+    });
+};
+export const GoBackOrHome = () => {
+    return Tools.comp("a", {
+        key: "goBack",
+        class: "flex items-center hover:underline",
+        children: [
+            Tools.comp("img", {
+                key: "img",
+                class: "w-8 h-8",
+                alt: "Logo",
+            }),
+        ],
+        href: "/",
+    });
+};
