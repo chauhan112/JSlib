8983db8ddb8f0ac070c3581b4f266e0790e61bec
raja <rajababuchauhan500@gmail.com>
2025-05-31 20:34:56 +0200

    implement and debugged the parse to string ( its working for few tests)

--- timeline/t2025/may/HtmlToMyLib/index.ts
+++ timeline/t2025/may/HtmlToMyLib/index.ts
@@ -55,4 +55,72 @@ export class HTMLParseAndMyLib {
 
         return Tools.comp(tagName, attrs);
     }
+    parseToString() {
+        let ele = this.node!.body;
+        return this._parse2String(ele);
+    }
+    _parse2String(ele: HTMLElement): string {
+        let attrs: any = {};
+        for (const attr of ele.attributes) {
+            if (ALLOWED_ATTRIBUTES.has(attr.name)) {
+                attrs[attr.name] = attr.value;
+            } else {
+                this.unprocessAttrs.add(attr.name);
+            }
+        }
+
+        let tagName = ele.tagName.toLocaleLowerCase();
+        if (tagName === "body") tagName = "div";
+        if (tagName === "svg") return `Tools.comp("div", { innerHTML: "svg" })`;
+
+        let processedChildren: (HTMLElement | string)[] = [];
+        for (const childNode of Array.from(ele.childNodes)) {
+            if (childNode.nodeType === Node.ELEMENT_NODE) {
+                const childElement = childNode as HTMLElement;
+                if (childElement.tagName === "SCRIPT") continue;
+                processedChildren.push(this._parse2String(childElement));
+            } else if (childNode.nodeType === Node.TEXT_NODE) {
+                let va = (childNode as Text).textContent;
+                if (va) {
+                    va = va.trim();
+                }
+                if (va !== "") attrs["textContent"] = va;
+            }
+        }
+        console.log("processedChildren", processedChildren);
+        if (processedChildren.length > 0) {
+            return `Tools.comp("${tagName}", { ${this.keyValToString(
+                attrs
+            )} children: [${processedChildren.join(",")}] })`;
+        }
+
+        return `Tools.comp("${tagName}", {${this.keyValToString(attrs)}})`;
+    }
+    keyValToString(keyVal: { [key: string]: string }): string {
+        let res: string[] = [];
+        for (const key in keyVal) {
+            if (keyVal.hasOwnProperty(key)) {
+                res.push(`${key}: "${keyVal[key]}"`);
+            }
+        }
+        let resStr = res.join(", ");
+        if (resStr.length > 0) {
+            resStr += ", ";
+        }
+        return resStr;
+    }
+}
+
+export class TestCases {
+    static test1() {
+        const html = `<div class="test" for="input1" placeholder="Enter text">
+                        <span>Text content</span>
+                        <input type="text" required />
+                      </div>`;
+        const parser = new HTMLParseAndMyLib();
+        parser.set_text(html);
+        const code = parser.parseCode();
+        console.log(code);
+        console.log("Unprocessed attributes:", parser.unprocessAttrs);
+    }
 }
