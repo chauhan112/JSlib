c1a63ab39ead75af4c46b857c5542e5c4325a3c3
raja <rajababuchauhan500@gmail.com>
2025-06-16 23:27:19 +0200

    scg creator and wrapper around main component

---
+++ timeline/t2025/june/domain-ops/SVGCreator.ts
@@ -0,0 +1,44 @@
+import { Tools } from "../../april/tools";
+const SVG_NAMESPACE = "http://www.w3.org/2000/svg";
+
+export const createNS = (name: string, props?: { [key: string]: any }) => {
+    let component = document.createElementNS(SVG_NAMESPACE, name);
+    if (props) {
+        for (let key in props) {
+            component.setAttribute(key, props[key]);
+        }
+    }
+    return component;
+};
+
+export const SVGCreator = (props?: { [key: string]: any }) => {
+    let component = createNS("svg");
+    const updateProp = (key: string, value: any) => {
+        if (key === "child") {
+            component.appendChild(value);
+            return;
+        }
+        if (key === "children") {
+            for (let child of value) {
+                component.appendChild(child);
+            }
+            return;
+        }
+        component.setAttribute(key, value);
+    };
+    for (let key in props) {
+        updateProp(key, props[key]);
+    }
+
+    let ele = Tools.div(
+        {},
+        {},
+        {
+            comp: component,
+            updateProp: updateProp,
+        }
+    );
+    ele.getElement().appendChild(component);
+
+    return ele;
+};
