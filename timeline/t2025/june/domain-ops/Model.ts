b92dea42f42aafe6ff3e65501cc1050d74b873f1
raja <rajababuchauhan500@gmail.com>
2025-06-18 21:58:07 +0200

    added local storage model for the domain ops

---
+++ timeline/t2025/june/domain-ops/Model.ts
@@ -0,0 +1,36 @@
+import { LocalStorageJSONModel } from "../../april/LocalStorage";
+import { v4 as uuidv4 } from "uuid";
+export class Model {
+    model = new LocalStorageJSONModel("dom-ops-logger");
+    create(name: string, loc: string[], typ = "domains") {
+        if (this.exists(name, loc, typ)) {
+            throw new Error("Domain already exists");
+        }
+        let idd = uuidv4();
+        let newLoc = [...loc, "domains", idd];
+        this.model.addEntry(newLoc, { name: name });
+    }
+    rename(newName: string, loc: string[]) {
+        if (!this.model.exists([...loc, "name"])) return;
+        this.model.updateEntry([...loc, "name"], newName);
+    }
+    delete(loc: string[]) {
+        this.model.deleteEntry(loc);
+    }
+    read(loc: string[]) {
+        return this.model.readEntry(loc);
+    }
+    exists(name: string, loc: string[], typ = "domains") {
+        let vals = this.model.readEntry([...loc, typ]);
+        for (let id in vals) {
+            if (vals[id]["name"] == name) {
+                return true;
+            }
+        }
+        return false;
+    }
+    readAll(loc: string[], typ = "domains") {
+        let vals = this.model.readEntry([...loc, typ]);
+        return vals;
+    }
+}
