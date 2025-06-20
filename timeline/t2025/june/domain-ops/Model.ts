7e0b793efdef7a2a255d49c2d053f99082d119a7
raja <rajababuchauhan500@gmail.com>
2025-06-20 12:04:09 +0200

    implemented simple crud for the domain and operations

--- timeline/t2025/june/domain-ops/Model.ts
+++ timeline/t2025/june/domain-ops/Model.ts
@@ -2,26 +2,32 @@ import { LocalStorageJSONModel } from "../../april/LocalStorage";
 import { v4 as uuidv4 } from "uuid";
 export class Model {
     model = new LocalStorageJSONModel("dom-ops-logger");
-    create(name: string, loc: string[], typ = "domains") {
-        if (this.exists(name, loc, typ)) {
-            throw new Error("Domain already exists");
-        }
-        let idd = uuidv4();
-        let newLoc = [...loc, "domains", idd];
-        this.model.addEntry(newLoc, { name: name });
+    domain: Domain = new Domain();
+    operations: Operations = new Operations();
+    activity: Activity = new Activity();
+    logger: Logger = new Logger();
+    logStructure: LogStructure = new LogStructure();
+    properties: Properties = new Properties();
+    searchFilters: SearchFilters = new SearchFilters();
+    constructor() {
+        this.domain.setModel(this.model);
+        this.operations.setModel(this.model);
+        this.activity.setModel(this.model);
+        this.logger.setModel(this.model);
+        this.logStructure.setModel(this.model);
+        this.properties.setModel(this.model);
+        this.searchFilters.setModel(this.model);
     }
-    rename(newName: string, loc: string[]) {
-        if (!this.model.exists([...loc, "name"])) return;
-        this.model.updateEntry([...loc, "name"], newName);
-    }
-    delete(loc: string[]) {
-        this.model.deleteEntry(loc);
-    }
-    read(loc: string[]) {
-        return this.model.readEntry(loc);
-    }
-    exists(name: string, loc: string[], typ = "domains") {
-        let vals = this.model.readEntry([...loc, typ]);
+}
+
+export class Tools {
+    static exists(
+        name: string,
+        loc: string[],
+        model: LocalStorageJSONModel,
+        typ = "domains"
+    ) {
+        let vals = model.readEntry([...loc, typ]);
         for (let id in vals) {
             if (vals[id]["name"] == name) {
                 return true;
@@ -29,34 +35,69 @@ export class Model {
         }
         return false;
     }
-    readAll(loc: string[], typ = "domains") {
-        let vals = this.model.readEntry([...loc, typ]);
-        return vals;
-    }
 }
 
 export class Domain {
-    create(loc: string[], name: string) {}
-    read(loc: string[]) {}
-    updateName(loc: string[], name: string) {}
-    delete(loc: string[]) {}
+    model: LocalStorageJSONModel | null = null;
+    key: string = "domains";
+    setModel(model: LocalStorageJSONModel) {
+        this.model = model;
+    }
+    create(loc: string[], name: string) {
+        if (this.exists(name, loc)) {
+            throw new Error("Domain already exists");
+        }
+        let idd = uuidv4();
+        let newLoc = [...loc, this.key, idd];
+        this.model?.addEntry(newLoc, { name: name });
+    }
+    read(id: string, loc: string[]) {
+        if (!this.model?.exists([...loc, this.key, id])) {
+            throw new Error("Domain does not exist");
+        }
+        return this.model.readEntry([...loc, this.key, id]);
+    }
+    readAll(loc: string[]) {
+        return this.model?.readEntry([...loc, this.key]);
+    }
+    updateName(loc: string[], name: string) {
+        if (!this.model?.exists([...loc, "name"])) return;
+        this.model.updateEntry([...loc, "name"], name);
+    }
+    delete(id: string, loc: string[]) {
+        if (!this.model?.exists([...loc, this.key, id])) {
+            throw new Error("Domain does not exist");
+        }
+        this.model.deleteEntry([...loc, this.key, id]);
+    }
+    exists(name: string, loc: string[]) {
+        return Tools.exists(name, loc, this.model!, this.key);
+    }
 }
 
-export class Operations {
-    create(loc: string[], name: string) {}
-    read(loc: string[]) {}
-    updateName(loc: string[], name: string) {}
-    delete(loc: string[]) {}
+export class Operations extends Domain {
+    key: string = "operations";
 }
 
 export class Activity {
+    model: LocalStorageJSONModel | null = null;
+    setModel(model: LocalStorageJSONModel) {
+        this.model = model;
+    }
     create(loc: string[], name: string) {}
     read(loc: string[]) {}
     updateName(loc: string[], name: string) {}
     delete(loc: string[]) {}
+    exists(name: string, loc: string[]) {
+        return Tools.exists(name, loc, this.model!, "activity");
+    }
 }
 
 export class Logger {
+    model: LocalStorageJSONModel | null = null;
+    setModel(model: LocalStorageJSONModel) {
+        this.model = model;
+    }
     create(loc: string[], name: string) {}
     read(loc: string[]) {}
     updateName(loc: string[], name: string) {}
@@ -64,6 +105,10 @@ export class Logger {
 }
 
 export class LogStructure {
+    model: LocalStorageJSONModel | null = null;
+    setModel(model: LocalStorageJSONModel) {
+        this.model = model;
+    }
     create(loc: string[], name: string) {}
     read(loc: string[]) {}
     updateName(loc: string[], name: string) {}
@@ -71,6 +116,10 @@ export class LogStructure {
 }
 
 export class Properties {
+    model: LocalStorageJSONModel | null = null;
+    setModel(model: LocalStorageJSONModel) {
+        this.model = model;
+    }
     create(loc: string[], name: string) {}
     read(loc: string[]) {}
     updateName(loc: string[], name: string) {}
@@ -78,6 +127,10 @@ export class Properties {
 }
 
 export class SearchFilters {
+    model: LocalStorageJSONModel | null = null;
+    setModel(model: LocalStorageJSONModel) {
+        this.model = model;
+    }
     create(loc: string[], name: string) {}
     read(loc: string[]) {}
     updateName(loc: string[], name: string) {}
