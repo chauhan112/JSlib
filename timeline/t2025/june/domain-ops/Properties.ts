e3d5592f7a085ac7e65ac476e06dfefc305fac65
raja <rajababuchauhan500@gmail.com>
2025-06-23 21:03:58 +0200

    refactored code

---
+++ timeline/t2025/june/domain-ops/Properties.ts
@@ -0,0 +1,56 @@
+import { Tools } from "../../april/tools";
+import { SmallCRUDops } from "./SimpleCrudOps";
+import {
+    Test,
+    AttributeForm,
+    PropertySection,
+} from "../../april/DomainOps/PropertySection";
+import { Plus } from "lucide";
+import { GlobalStates } from "./GlobalStates";
+export const CreateForm = () => {};
+
+export const Properties = () => {
+    const props = new PropertySection();
+    props.getElement();
+    const header = Header();
+    const table = Tools.container({
+        class: "p-2 w-full text-white",
+        child: Test.table(),
+    });
+    const form = new AttributeForm();
+    form.getElement();
+    let comp = Tools.div({
+        class: "flex flex-col items-center w-fit bg-[#1ABC9C] h-full",
+        children: [header, table],
+    });
+    form.s.comps.closeBtn.getElement().classList.add("hidden");
+
+    header.s.plus.update(
+        {},
+        {
+            click: (e: any, ls: any) => {
+                let modal = GlobalStates.getInstance().getState("modal");
+                modal.s.modalTitle.update({ textContent: "Create Attribute" });
+                modal.s.handlers.display(form);
+                modal.s.handlers.show();
+            },
+        }
+    );
+    return comp;
+};
+
+const Header = () => {
+    return Tools.div({
+        class: "flex bg-slate-700 gap-2 font-bold items-center w-80  border-white border-b",
+        children: [
+            Tools.div({
+                class: "bg-slate-700 py-2 text-xl font-bold w-full text-center text-white",
+                textContent: "Properties",
+            }),
+            Tools.icon(Plus, {
+                key: "plus",
+                class: " text-white hover:text-gray-300 cursor-pointer w-8 h-8 mr-2",
+            }),
+        ],
+    });
+};
---
+++ timeline/t2025/june/domain-ops/SimpleCrudOps.ts
@@ -0,0 +1,51 @@
+import { Tools } from "../../april/tools";
+import { GComponent } from "../../april/GComponent";
+import { NavChild } from "./Component";
+export const SmallCRUDops = (ops: any[], form: GComponent) => {
+    form.getElement().classList.add("hidden");
+
+    const onMainBodyClick = (e: any, ls: any) => {};
+    const onMenuOptionClick = (e: any, ls: any) => {};
+    let state = {
+        onMainBodyClick,
+        onMenuOptionClick,
+    };
+    const getNavItem = (item: any) => {
+        return NavChild({
+            ...item,
+            onMainBodyClick: (e: any, ls: any) => {
+                state.onMainBodyClick(e, ls);
+            },
+            onMenuOptionClick: (e: any, ls: any) => {
+                state.onMenuOptionClick(e, ls);
+            },
+        });
+    };
+    const navItem = Tools.div({
+        class: "w-full flex flex-col items-center px-2 gap-2",
+        key: "navItems",
+        children: ops.map(getNavItem),
+    });
+    const updateNavItems = (items: any[]) => {
+        navItem.update({
+            innerHTML: "",
+            children: items.map(getNavItem),
+        });
+    };
+    return Tools.div(
+        {
+            class: "w-full",
+            children: [
+                Tools.comp("button", {
+                    key: "createBtn",
+                    textContent: "+ create new",
+                    class: "text-2xl w-full flex items-center justify-center py-4 hover:border cursor-pointer",
+                }),
+                form,
+                navItem,
+            ],
+        },
+        {},
+        { updateNavItems, state }
+    );
+};
