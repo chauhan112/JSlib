8098f2a9e22a15d68efe389b2b5a728b3cacea43
raja <rajababuchauhan500@gmail.com>
2025-07-02 12:54:34 +0200

    classes for the grid table

--- timeline/t2025/june/domain-ops/ActivityLogger/LoggerData.ts
+++ timeline/t2025/june/domain-ops/ActivityLogger/LoggerData.ts
@@ -20,7 +20,21 @@ export const Unstructured = () => {
 };
 
 export const ColsSizes = {
-    six_cols: "grid grid-cols-6 gap-4",
+    six_cols: {
+        class: "grid grid-cols-6 gap-4",
+    },
+    did_next_cols_header: {
+        class: "grid grid-cols-[40px_2fr_2fr_1fr_1fr_100px] gap-4 px-4 py-2 bg-[#6D5E4B] text-white rounded-t-md font-medium text-sm sticky top-0 mt-6",
+    },
+    did_next_cols_data: {
+        class: "grid grid-cols-[40px_2fr_2fr_1fr_1fr_100px] gap-4 px-4 py-3 items-center border-b border-[#E7DAB8]/70 text-sm text-gray-700 last:border-b-0",
+    },
+    action: {
+        class: "hover:cursor-pointer hover:scale-110 transition-all duration-300",
+    },
+    actionWrapper: {
+        class: "flex justify-end space-x-3 text-[#4A8C71]",
+    },
 };
 
 export const Controller = (root: any) => {
