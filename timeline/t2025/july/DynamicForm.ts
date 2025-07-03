cf0099f862041dd0c6a8e6a54650bc26effbeff0
raja <rajababuchauhan500@gmail.com>
2025-07-03 14:29:32 +0200

    dynamic form and default params

--- timeline/t2025/july/DynamicForm.ts
+++ timeline/t2025/july/DynamicForm.ts
@@ -1,27 +1,47 @@
 import { GenericForm, Params } from "../june/domain-ops/Form";
 import { InputType } from "../june/domain-ops/Model";
 
-export const DynamicForm = () => {
-    const form = GenericForm();
-    let options = Object.entries(InputType);
-    let comps: any[] = [
-        Params.inp("key", {
-            class: "w-full p-2 rounded-md bg-gray-100 text-black",
-            placeholder: "Enter the key",
-        }),
-        Params.select(
-            "type",
-            options.map((op) => [op[1], op[0]])
-        ),
-        Params.inp("order", {
-            class: "w-full p-2 rounded-md bg-gray-100 text-black",
-            placeholder: "give the order for sorting",
-            type: "number",
-            value: 0,
-        }),
-        Params.inpSubmit(),
-    ];
+export const DynamicFormController = () => {
+    const comp = GenericForm();
 
-    form.s.handlers.setComponents(comps);
-    return form;
+    const applyIfNotExists = (obj: any, key: string, val: any) => {
+        if (obj && !obj[key]) {
+            obj[key] = val;
+        }
+    };
+    const setFields = (
+        fields: { type: InputType; key: string; params: any }[]
+    ) => {
+        let fieldComps: any = [];
+        let defValues = {
+            inp: {
+                class: "w-full p-2 rounded-md bg-gray-100 text-black",
+            },
+            textArea: {
+                class: "w-full p-2 rounded-md bg-gray-100 text-black flex-1",
+            },
+        };
+
+        for (const field of fields) {
+            if (!field.key) {
+                continue;
+            }
+            let params = { ...field.params };
+            if (field.type == InputType.Input) {
+                applyIfNotExists(params, "class", defValues.inp.class);
+                applyIfNotExists(params, "placeholder", "Enter " + field.key);
+                fieldComps.push(Params.inp(field.key, params));
+            } else if (field.type == InputType.Select) {
+                fieldComps.push(Params.select(field.key, field.params));
+            } else if (field.type == InputType.LargeText) {
+                applyIfNotExists(params, "class", defValues.textArea.class);
+                applyIfNotExists(params, "placeholder", "Enter " + field.key);
+                fieldComps.push(Params.textArea(field.key, params));
+            }
+        }
+        fieldComps.push(Params.inpSubmit());
+        comp.s.handlers.setComponents(fieldComps);
+    };
+
+    return { comp, setFields };
 };
