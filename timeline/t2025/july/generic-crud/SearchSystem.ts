8f30466748a81f889b4f4d9846535e8bcca6947b
raja <rajababuchauhan500@gmail.com>
2025-07-07 00:51:49 +0200

    refactored and organised the code

--- timeline/t2025/july/generic-crud/SearchSystem.ts
+++ timeline/t2025/july/generic-crud/SearchSystem.ts
@@ -36,6 +36,17 @@ export class Comparer {
     static isArr(obj: any) {
         return Array.isArray(obj);
     }
+    static caseRegexCompare(
+        wordToSearch: string,
+        text: string,
+        reg: boolean = false,
+        caseSensitive: boolean = false
+    ) {
+        if (caseSensitive)
+            return Comparer.compare(wordToSearch, text, ComparerType.in);
+        if (reg) Comparer.compare(wordToSearch, text, ComparerType.regex);
+        return Comparer.compare(wordToSearch, text, ComparerType.caseless);
+    }
 }
 
 class DataSetter {
@@ -47,84 +58,8 @@ class DataSetter {
         this.data = data;
     }
 }
-export class DicSearchSystem extends DataSetter {
-    siftSearch(params: any) {
-        let vals = Object.values(this.data);
-        return vals.filter(sift(params));
-    }
-    stringSearch(
-        text: string,
-        caseSensitive: boolean = false,
-        reg: boolean = false
-    ) {
-        let vals = Object.values(this.data);
-        if (text === "") return vals;
-        return vals.filter((val: any) => {
-            let value = JSON.stringify(val);
-            return this.caseRegexCompare(text, value, reg, caseSensitive);
-        });
-    }
-    caseRegexCompare(
-        wordToSearch: string,
-        text: string,
-        reg: boolean = false,
-        caseSensitive: boolean = false
-    ) {
-        if (caseSensitive)
-            return Comparer.compare(wordToSearch, text, ComparerType.in);
-        if (reg) Comparer.compare(wordToSearch, text, ComparerType.regex);
-        return Comparer.compare(wordToSearch, text, ComparerType.caseless);
-    }
-    keyValSearch(
-        text: string,
-        caseSensitive: boolean = false,
-        reg: boolean = false
-    ) {
-        let vals = Object.entries(this.data);
-        return vals.filter((val: any) => {
-            let value = JSON.stringify(val);
-            if (caseSensitive)
-                return Comparer.compare(text, value, ComparerType.in);
-            if (reg) Comparer.compare(text, value, ComparerType.regex);
-            return Comparer.compare(text, value, ComparerType.caseless);
-        });
-    }
-    locSearch(
-        loc: string[],
-        params: { word: string; case: boolean; reg: boolean }
-    ) {
-        let res = [];
-        for (const key in this.data) {
-            if (loc.length === 0) {
-                if (
-                    this.caseRegexCompare(
-                        params.word,
-                        key,
-                        params.reg,
-                        params.case
-                    )
-                ) {
-                    res.push(this.data[key]);
-                }
-            } else {
-                try {
-                    // if loc exists
-                    let val = DicSearchSystem.readEntry(loc, this.data[key]);
-                    if (
-                        this.caseRegexCompare(
-                            params.word,
-                            val,
-                            params.reg,
-                            params.case
-                        )
-                    ) {
-                        res.push(this.data[key]);
-                    }
-                } catch (e) {}
-            }
-        }
-        return res;
-    }
+
+export class DicOperation {
     static readEntry(location: string[], data: any) {
         let x = data;
         for (let key of location) {
@@ -135,9 +70,92 @@ export class DicSearchSystem extends DataSetter {
         }
         return x;
     }
+    static exists(location: string[], data: any) {
+        let x = data;
+        for (let key of location) {
+            if (!x.hasOwnProperty(key)) {
+                return false;
+            }
+            x = x[key];
+        }
+        return true;
+    }
 }
-export class ArraySearch extends DataSetter {
-    search(params: any) {}
+
+export class DicSearchSystem extends DataSetter {
+    siftSearch(params: any) {
+        return ArraySearch.siftSearch(params, this.data);
+    }
+    stringSearch(
+        text: string,
+        caseSensitive: boolean = false,
+        reg: boolean = false
+    ) {
+        return ArraySearch.stringSearch(this.data, text, caseSensitive, reg);
+    }
+
+    keyValSearch(
+        text: string,
+        caseSensitive: boolean = false,
+        reg: boolean = false
+    ) {
+        return ArraySearch.stringSearch(
+            Object.entries(this.data),
+            text,
+            caseSensitive,
+            reg
+        );
+    }
+    locSearch(
+        loc: string[],
+        params: { word: string; case: boolean; reg: boolean }
+    ) {
+        let res = [];
+        for (const key in this.data) {
+            if (loc.length === 0) {
+                if (
+                    Comparer.caseRegexCompare(
+                        params.word,
+                        key,
+                        params.reg,
+                        params.case
+                    )
+                ) {
+                    res.push(this.data[key]);
+                }
+            } else if (DicOperation.exists(loc, this.data[key])) {
+                let val = DicOperation.readEntry(loc, this.data[key]);
+                if (
+                    Comparer.caseRegexCompare(
+                        params.word,
+                        val,
+                        params.reg,
+                        params.case
+                    )
+                ) {
+                    res.push(this.data[key]);
+                }
+            }
+        }
+        return res;
+    }
+}
+export class ArraySearch {
+    static siftSearch(params: any, data: any[]) {
+        return data.filter(sift(params));
+    }
+    static stringSearch(
+        data: any[],
+        text: string,
+        caseSensitive: boolean = false,
+        reg: boolean = false
+    ) {
+        if (text === "") return data;
+        return data.filter((val: any) => {
+            let value = JSON.stringify(val);
+            return Comparer.caseRegexCompare(text, value, reg, caseSensitive);
+        });
+    }
     sequenceSearch(params: any) {}
 }
 export class Sorter extends DataSetter {
