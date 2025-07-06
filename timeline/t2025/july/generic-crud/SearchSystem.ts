560847bf66618dee1ef6b33852c83d6c2358ddc4
raja <rajababuchauhan500@gmail.com>
2025-07-07 00:42:28 +0200

    implemented loc search

--- timeline/t2025/july/generic-crud/SearchSystem.ts
+++ timeline/t2025/july/generic-crud/SearchSystem.ts
@@ -61,11 +61,19 @@ export class DicSearchSystem extends DataSetter {
         if (text === "") return vals;
         return vals.filter((val: any) => {
             let value = JSON.stringify(val);
-            if (caseSensitive)
-                return Comparer.compare(text, value, ComparerType.in);
-            if (reg) Comparer.compare(text, value, ComparerType.regex);
-            return Comparer.compare(text, value, ComparerType.caseless);
+            return this.caseRegexCompare(text, value, reg, caseSensitive);
         });
+    }
+    caseRegexCompare(
+        wordToSearch: string,
+        text: string,
+        reg: boolean = false,
+        caseSensitive: boolean = false
+    ) {
+        if (caseSensitive)
+            return Comparer.compare(wordToSearch, text, ComparerType.in);
+        if (reg) Comparer.compare(wordToSearch, text, ComparerType.regex);
+        return Comparer.compare(wordToSearch, text, ComparerType.caseless);
     }
     keyValSearch(
         text: string,
@@ -80,6 +88,52 @@ export class DicSearchSystem extends DataSetter {
             if (reg) Comparer.compare(text, value, ComparerType.regex);
             return Comparer.compare(text, value, ComparerType.caseless);
         });
+    }
+    locSearch(
+        loc: string[],
+        params: { word: string; case: boolean; reg: boolean }
+    ) {
+        let res = [];
+        for (const key in this.data) {
+            if (loc.length === 0) {
+                if (
+                    this.caseRegexCompare(
+                        params.word,
+                        key,
+                        params.reg,
+                        params.case
+                    )
+                ) {
+                    res.push(this.data[key]);
+                }
+            } else {
+                try {
+                    // if loc exists
+                    let val = DicSearchSystem.readEntry(loc, this.data[key]);
+                    if (
+                        this.caseRegexCompare(
+                            params.word,
+                            val,
+                            params.reg,
+                            params.case
+                        )
+                    ) {
+                        res.push(this.data[key]);
+                    }
+                } catch (e) {}
+            }
+        }
+        return res;
+    }
+    static readEntry(location: string[], data: any) {
+        let x = data;
+        for (let key of location) {
+            if (!x.hasOwnProperty(key)) {
+                throw new Error("Key does not exist");
+            }
+            x = x[key];
+        }
+        return x;
     }
 }
 export class ArraySearch extends DataSetter {
