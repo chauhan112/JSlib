a83b894f085e0fd26df03604ddf7cdc7e8fc1885
raja <rajababuchauhan500@gmail.com>
2025-07-11 18:09:17 +0200

    made the component visible and good looking

--- timeline/t2025/july/generic-webpage/Component.ts
+++ timeline/t2025/july/generic-webpage/Component.ts
@@ -3,125 +3,138 @@ import {
     BarChart,
     ChevronDown,
     History,
+    IconNode,
     Key,
     MessageSquareDot,
     MessageSquareMore,
-    PanelLeftOpen,
+    PanelLeftClose,
 } from "lucide";
 import { Tools } from "../../april/tools";
+import { GComponent } from "../../april/GComponent";
+
+export const Accordion = () => {
+    const comp = Tools.comp("div", {
+        class: "accordion-item",
+    });
+    const states: any = {
+        body: Tools.div({ class: "hidden" }),
+    };
+    const setBodyHeader = (body: GComponent, header: GComponent) => {
+        body.getElement().classList.add("hidden");
+        header.update(
+            {},
+            {
+                click: (e: any) => {
+                    body.getElement().classList.toggle("hidden");
+                    e.stopPropagation();
+                },
+            }
+        );
+        comp.update({ innerHTML: "", children: [header, body] });
+        states.body = body;
+        states.header = header;
+    };
+
+    const setLabel = (label: string) => {
+        let downIcon = Tools.icon(ChevronDown, {
+            class: "w-6 h-6 transition duration-300",
+        });
+        let header = Tools.comp(
+            "button",
+            {
+                class: "cursor-pointer flex w-full items-center justify-between rounded-lg px-3 py-2 text-sm font-medium hover:bg-[#dde3ea] dark:hover:bg-[#282a2c]",
+                children: [
+                    Tools.comp("span", { textContent: label }),
+                    downIcon,
+                ],
+            },
+            {
+                click: (e: any) => {
+                    states.body.getElement().classList.toggle("hidden");
+                    downIcon.getElement().classList.toggle("rotate-180");
+                },
+            }
+        );
+
+        comp.update({ innerHTML: "", children: [header, states.body] });
+        states.header = header;
+    };
+    comp.update({}, {}, { setBodyHeader, states, setLabel });
+    return comp;
+};
 
 export const Sidebar = () => {
-    const panelOpen = Tools.comp("button", {
+    const panelClose = Tools.comp("button", {
         class: "lg:block p-1 rounded-full hover:bg-gray-200 dark:hover:bg-gray-700 right-0 bottom-0 hidden",
-        children: [Tools.icon(PanelLeftOpen, { class: "w-6 h-6" })],
+        children: [Tools.icon(PanelLeftClose, { class: "w-6 h-6" })],
     });
-
-    const msgIcon = Tools.icon(MessageSquareMore, { class: "w-6 h-6" });
-
-    const histIcon = Tools.icon(History, { class: "w-6 h-6" });
-
-    const var_depth_5 = Tools.comp("button", {
-        class: "accordion-header flex w-full items-center justify-between rounded-lg px-3 py-2 text-sm font-medium hover:bg-[#dde3ea] dark:hover:bg-[#282a2c]",
-        children: [
-            Tools.comp("span", { class: "nav-text", textContent: "Studio" }),
-            Tools.icon(ChevronDown, { class: "w-6 h-6" }),
-        ],
-    });
-
-    const var_depth_6 = Tools.comp("div", {
-        class: "accordion-content hidden overflow-hidden transition-all duration-300",
-        children: [
-            Tools.comp("div", {
-                class: "pt-1 pl-5",
-                children: [
-                    Tools.comp("a", {
-                        href: "#",
-                        class: "flex items-center gap-3 rounded-full px-3 py-2 text-sm font-medium hover:bg-[#dde3ea] dark:hover:bg-[#282a2c]",
-                        children: [
-                            Tools.icon(MessageSquareDot, { class: "w-6 h-6" }),
-                            Tools.comp("span", {
-                                class: "nav-text",
-                                textContent: "Chat",
-                            }),
-                        ],
-                    }),
-                    Tools.comp("a", {
-                        href: "#",
-                        class: "flex items-center gap-3 rounded-full px-3 py-2 text-sm font-medium hover:bg-[#dde3ea] dark:hover:bg-[#282a2c]",
-                        children: [
-                            Tools.icon(AudioLines, { class: "w-6 h-6" }),
-                            Tools.comp("span", {
-                                class: "nav-text",
-                                textContent: "Stream",
-                            }),
-                        ],
-                    }),
-                ],
+    const getAccordionBody = (items: { label: string; icon: IconNode }[]) => {
+        return Tools.comp("div", {
+            class: "pt-1 pl-5 hidden",
+            children: items.map((item) => {
+                return Tools.comp("a", {
+                    href: "#",
+                    class: "flex items-center gap-3 rounded-full px-3 py-2 text-sm font-medium hover:bg-[#dde3ea] dark:hover:bg-[#282a2c]",
+                    children: [
+                        Tools.icon(item.icon, { class: "w-6 h-6" }),
+                        Tools.comp("span", {
+                            textContent: "Chat",
+                        }),
+                    ],
+                });
             }),
-        ],
-    });
-
-    const var_depth_7 = Tools.comp("button", {
-        class: "accordion-header flex w-full items-center justify-between rounded-lg px-3 py-2 text-sm font-medium hover:bg-[#dde3ea] dark:hover:bg-[#282a2c]",
-        children: [
-            Tools.comp("span", { class: "nav-text", textContent: "Dashboard" }),
-            Tools.icon(ChevronDown, { class: "w-6 h-6" }),
-        ],
-    });
-
-    const var_depth_8 = Tools.comp("div", {
-        class: "accordion-content hidden overflow-hidden transition-all duration-300",
-        children: [
-            Tools.comp("div", {
-                class: "pt-1 pl-5",
-                children: [
-                    Tools.comp("a", {
-                        href: "#",
-                        class: "flex items-center gap-3 rounded-full px-3 py-2 text-sm font-medium hover:bg-[#dde3ea] dark:hover:bg-[#282a2c]",
-                        children: [
-                            Tools.icon(Key, { class: "w-6 h-6" }),
-                            Tools.comp("span", {
-                                class: "nav-text",
-                                textContent: "API Keys",
-                            }),
-                        ],
-                    }),
-                    Tools.comp("a", {
-                        href: "#",
-                        class: "flex items-center gap-3 rounded-full px-3 py-2 text-sm font-medium hover:bg-[#dde3ea] dark:hover:bg-[#282a2c]",
-                        children: [
-                            Tools.icon(BarChart, { class: "w-6 h-6" }),
-                            Tools.comp("span", {
-                                class: "nav-text",
-                                textContent: "Usage",
-                            }),
-                        ],
-                    }),
-                ],
-            }),
-        ],
-    });
+        });
+    };
+    const getAccordion = (
+        label: string,
+        items: { label: string; icon: IconNode }[]
+    ) => {
+        let acc = Accordion();
+        acc.s.states.body = getAccordionBody(items);
+        acc.s.setLabel(label);
+        return acc;
+    };
 
     const var_theme_toggle = Tools.comp("button", {
         class: "flex w-full items-center gap-3 rounded-full px-3 py-2 text-sm font-medium hover:bg-[#dde3ea] dark:hover:bg-[#282a2c]",
         children: [
             Tools.comp("span", { class: "material-symbols-outlined" }),
             Tools.comp("span", {
-                class: "nav-text",
                 textContent: "Toggle Theme",
             }),
         ],
     });
 
-    const var_depth_9 = Tools.comp("img", { class: "h-6 w-6 rounded-full" });
-
-    const var_depth_10 = Tools.comp("span", {
-        class: "nav-text",
-        textContent: "My Account",
+    const userSetting = Tools.comp("img", {
+        class: "h-6 w-6 rounded-full",
+        alt: "Profile",
+        src: "https://lh3.googleusercontent.com/a/ACg8ocIrAAIi2TgWjsyclT42OwOqtnlT_YplxXi0ad5gk1zlX2EPJg=s64-cc",
     });
 
-    const var_sidebar = Tools.comp("aside", {
-        class: "left-0 z-10 fixed flex h-screen flex-col overflow-y-auto dark:bg-[#1e1f20] bg-[#f3f6fc] transition-all duration-300 ease-in-out -translate-x-full lg:translate-x-0 w-64",
+    const getNavElement = (
+        icon: IconNode,
+        label: string,
+        selected: boolean = false
+    ) => {
+        let comp = Tools.comp("a", {
+            href: "#",
+            class: "flex items-center gap-3 rounded-full px-3 py-2 text-sm font-medium hover:bg-[#dde3ea] dark:hover:bg-[#282a2c]",
+            children: [
+                Tools.icon(icon, { class: "w-6 h-6" }),
+                Tools.comp("span", {
+                    textContent: label,
+                }),
+            ],
+        });
+        if (selected)
+            comp.update({
+                class: "flex items-center gap-3 rounded-full px-3 py-2 text-sm font-medium bg-[#dde3ea] dark:bg-[#282a2c] text-blue-600 dark:text-blue-400 hover:bg-[#d5dce6] dark:hover:bg-[#3c3f43]",
+            });
+        return comp;
+    };
+
+    return Tools.comp("aside", {
+        class: "fixed text-white left-0 z-10 inset-0 flex h-screen flex-col overflow-y-auto dark:bg-[#1e1f20] bg-[#f3f6fc] transition-all duration-300 ease-in-out -translate-x-full lg:translate-x-0 w-64",
         children: [
             Tools.comp("div", {
                 class: "flex items-center justify-between gap-2 px-4 py-5",
@@ -129,15 +142,14 @@ export const Sidebar = () => {
                     Tools.comp("a", {
                         href: "#",
                         class: "flex items-center gap-2",
-                        textContent: "Company Icon",
                         children: [
                             Tools.comp("h1", {
-                                class: "text-xl font-semibold nav-text",
-                                textContent: "AI Studio",
+                                class: "text-xl font-semibold ",
+                                textContent: "Company Icon",
                             }),
                         ],
                     }),
-                    panelOpen,
+                    panelClose,
                 ],
             }),
             Tools.comp("div", {
@@ -146,39 +158,19 @@ export const Sidebar = () => {
                     Tools.comp("nav", {
                         class: "flex flex-col gap-1",
                         children: [
-                            Tools.comp("a", {
-                                href: "#",
-                                class: "flex items-center gap-3 rounded-full px-3 py-2 text-sm font-medium bg-[#dde3ea] dark:bg-[#282a2c] text-blue-600 dark:text-blue-400 hover:bg-[#d5dce6] dark:hover:bg-[#3c3f43]",
-                                children: [
-                                    msgIcon,
-                                    Tools.comp("span", {
-                                        class: "nav-text",
-                                        textContent: "New chat",
-                                    }),
-                                ],
-                            }),
-                            Tools.comp("a", {
-                                href: "#",
-                                class: "flex items-center gap-3 rounded-full px-3 py-2 text-sm font-medium hover:bg-[#dde3ea] dark:hover:bg-[#282a2c]",
-                                children: [
-                                    histIcon,
-                                    Tools.comp("span", {
-                                        class: "nav-text",
-                                        textContent: "History",
-                                    }),
-                                ],
-                            }),
+                            getNavElement(MessageSquareMore, "New Chat", true),
+                            getNavElement(History, "History"),
                             Tools.comp("hr", {
                                 class: "my-2 border-gray-300 dark:border-gray-600",
                             }),
-                            Tools.comp("div", {
-                                class: "accordion-item",
-                                children: [var_depth_5, var_depth_6],
-                            }),
-                            Tools.comp("div", {
-                                class: "accordion-item",
-                                children: [var_depth_7, var_depth_8],
-                            }),
+                            getAccordion("Studio", [
+                                { label: "Chat", icon: MessageSquareDot },
+                                { label: "Stream", icon: AudioLines },
+                            ]),
+                            getAccordion("Dashboard", [
+                                { label: "API Keys", icon: Key },
+                                { label: "Usage", icon: BarChart },
+                            ]),
                         ],
                     }),
                     Tools.comp("div", {
@@ -191,7 +183,12 @@ export const Sidebar = () => {
                             Tools.comp("a", {
                                 href: "#",
                                 class: "flex items-center gap-3 rounded-full px-3 py-2 text-sm font-medium mt-1 hover:bg-[#dde3ea] dark:hover:bg-[#282a2c]",
-                                children: [var_depth_9, var_depth_10],
+                                children: [
+                                    userSetting,
+                                    Tools.comp("span", {
+                                        textContent: "My Account",
+                                    }),
+                                ],
                             }),
                         ],
                     }),
@@ -199,6 +196,4 @@ export const Sidebar = () => {
             }),
         ],
     });
-
-    return var_sidebar;
 };
