6781b7dd214e09aaf0ac64dc8f80c431cd6ff331
raja <rajababuchauhan500@gmail.com>
2025-08-03 14:11:28 +0200

    integrated the compact modal with global states

--- timeline/t2025/june/domain-ops/GlobalStates.ts
+++ timeline/t2025/june/domain-ops/GlobalStates.ts
@@ -3,7 +3,7 @@ import { Tools } from "../../april/tools";
 import { GenericModal } from "../../may/FileSearch/Modal";
 import { ContextMenu } from "./ContextMenu";
 import { MultiLayerModalCtrl } from "../../july/generic-crud/multiLayerModal";
-
+import { CompactModalCtrl } from "../../aug/LinksOpener/CompactModel";
 export class GlobalStates {
     static instance: GlobalStates | null = null;
     private readonly comp: GComponent;
@@ -12,11 +12,18 @@ export class GlobalStates {
         let modal = GenericModal("");
         let mModalCtrl = MultiLayerModalCtrl();
         let contextMenu = ContextMenu([]);
+        let compactModal = CompactModalCtrl();
         this.addState("modal", modal);
         this.addState("contextMenu", contextMenu);
         this.addState("multiModal", mModalCtrl);
+        this.addState("compactModal", compactModal);
         this.comp = Tools.div({
-            children: [modal, contextMenu, mModalCtrl.modal],
+            children: [
+                modal,
+                contextMenu,
+                mModalCtrl.modal,
+                compactModal.modal,
+            ],
         });
         this.addToBody();
     }
