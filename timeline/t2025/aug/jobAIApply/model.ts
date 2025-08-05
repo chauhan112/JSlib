114e1c15ffd781e6f151a276987a3c7d86f5034c
raja <rajababuchauhan500@gmail.com>
2025-08-05 22:30:02 +0200

    connecting to backend wrapper

---
+++ timeline/t2025/aug/jobAIApply/model.ts
@@ -0,0 +1,47 @@
+const makePostRequest = (url: string, data: any) => {
+    return fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+    }).then((resp) => resp.json());
+};
+
+export const Model = () => {
+    const state: any = {
+        url: "http://127.0.0.1:8000/run/",
+    };
+    const setUrl = (url: string) => {
+        state.url = url;
+    };
+
+    const addJob = (title: string, description: string, url: string = "") => {
+        return makePostRequest(state.url, {
+            name: "addData",
+            params: ["Job", { title, description, link: url }],
+        });
+    };
+
+    const readAllJobs = () => {
+        return makePostRequest(state.url, {
+            name: "readAll",
+            params: ["Job"],
+        });
+    };
+
+    const deleteJob = (id: string) => {
+        return makePostRequest(state.url, {
+            name: "deleteDataWithId",
+            params: ["Job", id],
+        });
+    };
+
+    const updateJob = (id: string, newData: any) => {
+        if (!newData) return;
+        return makePostRequest(state.url, {
+            name: "updateData",
+            params: ["Job", id, { ...newData }],
+        });
+    };
+
+    return { addJob, readAllJobs, setUrl, deleteJob, updateJob };
+};
