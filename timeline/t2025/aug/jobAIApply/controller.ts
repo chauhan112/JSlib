e5f53f0ce97f55bfbdbbcf61aedbc7426f5d4fb7
raja <rajababuchauhan500@gmail.com>
2025-08-06 14:45:15 +0200

    changed the layout and Job component. reused generic crud component

--- timeline/t2025/aug/jobAIApply/controller.ts
+++ timeline/t2025/aug/jobAIApply/controller.ts
@@ -1,11 +1,15 @@
 import { Page, JobAddForm, JobCard, ShowJobDetails } from "./page";
+import { Tools } from "../../april/tools";
 import { GlobalStates } from "../../june/domain-ops/GlobalStates";
 import { Model } from "./model";
+import { GenericCRUDCtrl, CardComp } from "../../july/generic-crud/page";
+import { InputType } from "../../june/domain-ops/Model";
 export const PageCtrl = () => {
     let comp = Page();
     let addForm = JobAddForm();
     let model = Model();
     let jobDetails = ShowJobDetails();
+    let crudCtrl = GenericCRUDCtrl();
     const onJobFormSubmit = (e: any, ls: any) => {
         e.preventDefault();
         let vals = addForm.s.get_values();
@@ -13,8 +17,41 @@ export const PageCtrl = () => {
             (addForm.getElement() as HTMLFormElement).reset();
             let modal = GlobalStates.getInstance().getState("compactModal");
             modal.hide();
+            onJobListRender();
         });
-        onJobListRender();
+    };
+
+    const states: any = {
+        formStruc: [
+            {
+                type: "input",
+                key: "title",
+                order: 2,
+                params: {
+                    required: true,
+                    placeholder: "Job Title",
+                },
+            },
+            {
+                type: "largeText",
+                key: "description",
+                order: 4,
+                params: {
+                    required: true,
+                    placeholder: "Job Description",
+                },
+            },
+            {
+                oder: 6,
+                type: "input",
+                key: "link",
+                params: {
+                    placeholder: "Link to the job",
+                },
+
+                order: 6,
+            },
+        ],
     };
     const onJobAdd = (e: any, ls: any) => {
         let modal = GlobalStates.getInstance().getState("compactModal");
@@ -36,20 +73,76 @@ export const PageCtrl = () => {
     };
     const onJobListRender = async () => {
         let jobs = await model.readAllJobs();
-        comp.s.jobList.update({
+        crudCtrl.funcs.setData(jobs);
+    };
+    const mergeTheLayout = () => {
+        crudCtrl.comp.s.ops
+            .getElement()
+            .classList.remove(
+                "absolute",
+                "top-0",
+                "left-0",
+                "z-10",
+                "flex-col"
+            );
+        crudCtrl.comp.getElement().classList.remove("h-screen");
+        crudCtrl.comp.s.ops
+            .getElement()
+            .classList.add(
+                "items-center",
+                "justify-between",
+                "py-2",
+                "text-xl"
+            );
+        crudCtrl.comp.s.ops.s.inner.update({
             innerHTML: "",
-            children: jobs.map((job: any) => {
-                let jc = JobCard(job);
-                jc.update({}, { click: onJobShow }, { job });
-                jc.s.deleteIcon.update({}, { click: onJobDelete }, { job });
-                return jc;
-            }),
+            children: [crudCtrl.comp.s.searchIcon, crudCtrl.comp.s.plusIcon],
+        });
+        crudCtrl.comp.s.ops.update({
+            innerHTML: "",
+            children: [
+                Tools.div({
+                    child: crudCtrl.comp.s.lister.s.pagination,
+                }),
+                crudCtrl.comp.s.ops.s.inner,
+            ],
+        });
+        crudCtrl.comp.s.ops.s.inner
+            .getElement()
+            .classList.remove("justify-between");
+        crudCtrl.comp.s.ops.s.inner.getElement().classList.add("w-fit");
+        crudCtrl.comp.s.lister.update({
+            innerHTML: "",
+            child: crudCtrl.comp.s.lister.s.comp,
         });
     };
-    const onSetup = () => {
-        comp.s.searchTools.s.addBtn.update({}, { click: onJobAdd });
+
+    const getCardComp = (x: any) => {
+        let jc = states.previourCardComp(x);
+        jc.update({}, { click: onJobShow }, { job: x });
+        jc.getElement().classList.add(
+            "hover-lift",
+            "border",
+            "p-6",
+            "cursor-pointer",
+            "shadow-md"
+        );
+        jc.getElement().classList.remove("bg-gray-100", "p-5");
+        // jc.s.deleteIcon.update({}, { click: onJobDelete }, { job: x });
+        return jc;
+    };
+    const onSetup = async () => {
         addForm.s.submitBtn.update({}, { click: onJobFormSubmit });
         onJobListRender();
+        await crudCtrl.funcs.setup();
+        mergeTheLayout();
+        crudCtrl.paginationCtrl.setPageSize(100);
+        states.previourCardComp = crudCtrl.funcs.getCardComp;
+        crudCtrl.funcs.getCardComp = getCardComp;
+        let formCtrl = crudCtrl.formCtrl;
+        formCtrl.model.states.onChange = () => {};
+        crudCtrl.dataCrudCtrl.states.fields = states.formStruc;
+        comp.s.main.update({ innerHTML: "", child: crudCtrl.comp });
     };
     onSetup();
 
