5f155f6d22da809d33d83e384e9954e0f289c68b
raja <rajababuchauhan500@gmail.com>
2025-08-07 11:55:17 +0200

    mode structure defining to mkae it easier for debugging or building on top of it

--- timeline/t2025/aug/jobAIApply/model.ts
+++ timeline/t2025/aug/jobAIApply/model.ts
@@ -6,12 +6,28 @@ const makePostRequest = (url: string, data: any) => {
     }).then((resp) => resp.json());
 };
 
+export type ModelType = {
+    addJob: (title: string, description: string, url: string) => Promise<any>;
+    readAllJobs: () => Promise<any>;
+    deleteJob: (id: string) => Promise<any>;
+    updateJob: (id: string, newData: any) => Promise<any>;
+    setUrl: (url: string) => Promise<any>;
+    readJob: (id: string) => Promise<any>;
+};
+
 export const Model = () => {
     const state: any = {
         url: "http://127.0.0.1:8000/run/",
     };
     const setUrl = (url: string) => {
         state.url = url;
+    };
+
+    const readJob = (id: string) => {
+        return makePostRequest(state.url, {
+            name: "readAsDic",
+            params: ["Job", id],
+        });
     };
 
     const addJob = (title: string, description: string, url: string = "") => {
