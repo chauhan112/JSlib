b93b89b85c486bae4f4c9c520fbebfae9488e1ba
raja <rajababuchauhan500@gmail.com>
2025-08-13 12:26:40 +0200

    api call bug fixes

--- timeline/t2025/aug/LinksOpener/graphql_api.ts
+++ timeline/t2025/aug/LinksOpener/graphql_api.ts
@@ -1,55 +1,87 @@
-let APILoc = "https://localhost:8000/graphql";
+export let APILoc = "http://127.0.0.1:8000/graphql";
 
-class GraphApiCalls {
+export const makePostRequest = (url: string, data: any) => {
+    return fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+    }).then((resp) => resp.json());
+};
+
+export class GraphApiCalls {
     static async updateCollection(id: string, title: string) {
-        const query = `mutation{updateCollection(collectionInput: {id:"${id}",name:"${title}"})}`;
-        const resp = await fetch(APILoc, {
-            body: query,
+        const query = `
+            mutation UpdateCollection($id: ID!, $name: String!) {
+                updateCollection(
+                  collectionInput: {
+                    id: $id
+                    name: $name
+                  }
+                )
+            }
+        `;
+        const resp = await makePostRequest(APILoc, {
+            query,
+            variables: {
+                id,
+                name: title,
+            },
         });
-        return resp.json();
+        return resp;
     }
     static async readAllCollections() {
-        const resp = await fetch(APILoc, {
-            body: `query{allCollections{id name}}`,
+        const query = "{ allCollections {id name}}";
+        const resp = await makePostRequest(APILoc, {
+            query,
         });
-        return resp.json();
+        return resp;
     }
-    static async createCollectio(name: string) {
+    static async createCollection(name: string) {
+        // const query = `
+        //     mutation addCollection($name: String!) {
+        //         updateCollection(
+        //           collectionInput: {
+        //             id: $id
+        //             name: $name
+        //           }
+        //         )
+        //     }
+        // `;
         const query = `mutation{addCollection(collectionInput: {name:"${name}"}){id}}`;
-        const resp = await fetch(APILoc, {
-            body: query,
+        const resp = await makePostRequest(APILoc, {
+            query,
         });
-        return resp.json();
+        return resp;
     }
     static async deleteCollection(id: any) {
         const query = `mutation{deleteCollection(id:${id})}`;
-        const resp = await fetch(APILoc, {
-            body: query,
+        const resp = await makePostRequest(APILoc, {
+            query,
         });
-        return resp.json();
+        return resp;
     }
 
     static async deleteLink(id: any) {
         const query = `mutation{deleteLink(id:${id})}`;
-        const resp = await fetch(APILoc, {
-            body: query,
+        const resp = await makePostRequest(APILoc, {
+            query,
         });
-        return resp.json();
+        return resp;
     }
 
     static async updateLink(id: any, title: string, url: string) {
         const query = `mutation{deleteLink(id:${id}, title:"${title}", url:"${url}")}`;
-        const resp = await fetch(APILoc, {
-            body: query,
+        const resp = await makePostRequest(APILoc, {
+            query,
         });
-        return resp.json();
+        return resp;
     }
 
     static async createLink(collectionId: any, title: string, url: string) {
         const query = `mutation{addLink(linkInput:{collectionId:${collectionId}, title:"${title}", url:"${url}"}){id, title, url}}`;
-        const resp = await fetch(APILoc, {
-            body: query,
+        const resp = await makePostRequest(APILoc, {
+            query,
         });
-        return resp.json();
+        return resp;
     }
 }
