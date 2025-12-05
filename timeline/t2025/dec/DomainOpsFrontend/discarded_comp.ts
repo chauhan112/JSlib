057e246fe1bfb844f96dfc3b5110a93c3d390221
Raja <rajababuchauhan500@gmail.com>
2025-12-05 22:12:35 +0100

    testing and checking components

---
+++ timeline/t2025/dec/DomainOpsFrontend/discarded_comp.ts
@@ -0,0 +1,37 @@
+import { Tools } from "../../april/tools";
+import { FormSelectCompCtrl, FormTextareaCtrl } from "../../aug/jobAIApply/simpleVersion";
+import { GenericCRUD } from "../../july/generic-crud/page";
+import { CollectionForm } from "../../aug/LinksOpener/Page";
+
+export const OpsSection = () => {
+    const searchComp = FormSelectCompCtrl("dom op activities");
+    const options = [
+        { value: "domain", label: "Domain" },
+        { value: "operation", label: "Operation" },
+        {
+            value: "activity", label: "Activity"
+        }
+    ];
+    searchComp.setOptions(options);
+    searchComp.setLabel("Domain Ops Activities");
+    const nameComp = FormTextareaCtrl("name", "Name", "Enter a name for the domain, operation, or activity");
+    return Tools.div({
+        class: "w-full flex flex-col gap-2 p-2",
+        children: [
+            searchComp.comp,
+            nameComp.comp,
+        ]
+    });
+}
+
+export const SimpleLayout = () => {
+    const opsSection = GenericCRUD();
+    const collectionForm = CollectionForm();
+    return Tools.div({
+        class: "w-full flex gap-2 p-2",
+        children: [
+            opsSection,
+            collectionForm,
+        ]
+    });
+};
