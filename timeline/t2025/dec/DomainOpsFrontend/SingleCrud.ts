75da09197596aa5739a98bb7e1dfc07d9beec8ff
Raja <rajababuchauhan500@gmail.com>
2025-12-06 15:04:47 +0100

    Dynamic form manager another wrapper around wrapper but it does not matter because this makes sure that the component is created when required

--- timeline/t2025/dec/DomainOpsFrontend/SingleCrud.ts
+++ timeline/t2025/dec/DomainOpsFrontend/SingleCrud.ts
@@ -38,7 +38,6 @@ export class SingleCrudModel {
         this.data = this.data.filter((item: any) => item.id !== id);
     }
 }
-
 export const SingleCrud = () => {
     const searchComp = SearchComp();
     searchComp.getElement().classList.remove("p-4");
@@ -47,9 +46,60 @@ export const SingleCrud = () => {
     return Tools.div({
         class: "w-full flex-col flex gap-2 p-2 ",
         children: [searchComp, listDisplayer],
-    });
+    }, {}, { searchComp, listDisplayer });
 };
-
+export class DynamicFormManager {
+    fields: any[] = [];
+    dataFormCtrl: any = undefined;
+    currentId: any;
+    onSubmit: (data: any, id: any) => void = (data: any, id: any) => {
+        console.log(data, id);
+    };
+    set_fields(fields: any[]) {
+        this.fields = fields;
+    }
+    get_values() {
+        return this.dataFormCtrl.comp.s.handlers.getValues();
+    }
+    clear_values() {
+        this.dataFormCtrl.comp.s.handlers.clearValues();
+    }
+    set_values(id: any, values: any) {
+        this.dataFormCtrl.comp.s.handlers.setValues(values);
+        this.currentId = id;
+    }
+    private def_submit(e: any,) {
+        e.preventDefault();
+        this.onSubmit(this.get_values(), this.currentId);
+    }
+    setup() {
+        if (!this.dataFormCtrl) {
+            this.dataFormCtrl = DynamicFormController();
+            this.dataFormCtrl.comp.s.handlers.submit = this.def_submit.bind(this);
+            this.dataFormCtrl.setFields(this.fields);
+        }
+    }
+}
+export const ViewComponent = () => {
+    return Tools.comp("textarea", {
+        placeholder: "content goes here",
+        class: "h-64 w-full border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm",
+        disabled: true,
+    });
+}
+export class ViewController {
+    comp: any;
+    set_comp(comp: any) {
+        this.comp = comp;
+    }
+    set_data(data: any) {
+        (this.comp.getElement() as HTMLTextAreaElement).value = JSON.stringify(
+            data,
+            null,
+            2
+        );
+    }
+}
 export class SingleCrudController {
     comp: any;
     model: SingleCrudModel = new SingleCrudModel();
