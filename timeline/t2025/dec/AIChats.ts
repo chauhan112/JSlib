08d0f85ffdc59c6715d24d118d35dcd2230371ac
Raja <rajababuchauhan500@gmail.com>
2025-12-11 20:39:48 +0100

    AI Chats frontend app for searching in ai related chats

--- timeline/t2025/dec/AIChats.ts
+++ timeline/t2025/dec/AIChats.ts
@@ -4,7 +4,7 @@ import { MainCtrl as SingleCrudMainCtrl } from "./DomainOpsFrontend/SingleCrud";
 import { backendCall } from "./DomainOpsFrontend/api_calls";
 import { type SingleCrudModelInterface } from "./DomainOpsFrontend/SingleCrud";
 export class AIChatsCRUDModel implements SingleCrudModelInterface {
-    type: string = "AIchats";
+    type: string = "AIChats";
     
     async read_all (){
         let data = await backendCall("read_all", {}, this.type);
@@ -26,13 +26,19 @@ export class AIChatsCRUDModel implements SingleCrudModelInterface {
         let data = await backendCall("delete", {id}, this.type);
         return data.data;
     }
+    async open_chat(path: string){
+        let data = await backendCall("open_file", {path}, this.type);
+        return data.data;
+    }
 }
 export const AIChatsPage = () => {
-    const singleCrudCtrl = SingleCrudMainCtrl.singleCrud(4, new AIChatsCRUDModel(), (data: any) => data.name,
+    const singleCrudCtrl = SingleCrudMainCtrl.singleCrud(10, new AIChatsCRUDModel(), (data: any) => data.name,
         [{type: InputType.Input, key: "name", params: {placeholder: "Enter name"}}],
         [{type: InputType.Input, key: "name", params: {placeholder: "Enter name"}}]
     );
-    
+    singleCrudCtrl.on_card_clicked = (data: any) => {
+        (singleCrudCtrl.model as AIChatsCRUDModel).open_chat(data.path);
+    }
     return Tools.div({
         class: "w-full flex-col flex gap-2 p-2 ",
         children: [singleCrudCtrl.comp],
