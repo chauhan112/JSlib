e47be59992dbe094f7ad9ecca8153485396b6283
Raja <rajababuchauhan500@gmail.com>
2025-12-11 20:58:37 +0100

    activity crud adding

--- timeline/t2025/dec/DomainOpsFrontend/index.ts
+++ timeline/t2025/dec/DomainOpsFrontend/index.ts
@@ -1,9 +1,9 @@
 import { MainCtrl as RouteWebPageMainCtrl } from "./route/controller";
 import { DomainOpsPage, DomainOpsCRUDModel } from "./pages/dom_op_page";
-
+import { ActivityPage } from "./pages/activity";
 const domOps = DomainOpsPage();
 export const Page = () => {
-    const menus = [{label: "Domain", href: "/domain"}, {label: "Operation", href: "/operation"}];
+    const menus = [{label: "Domain", href: "/domain"}, {label: "Operation", href: "/operation"}, {label: "Activity", href: "/activity"}];
     const routeWebPageCtrl = RouteWebPageMainCtrl.routeWebPage(menus, [{ href: "/domain", page : () => {
         (domOps.s.singleCrudCtrl.model as DomainOpsCRUDModel).type = "Domain";
         domOps.s.singleCrudCtrl.update();
@@ -14,6 +14,8 @@ export const Page = () => {
             domOps.s.singleCrudCtrl.update();
             return domOps;
         }
-    }]);
+    }])
+
+    routeWebPageCtrl.add_route_page("/activity", ActivityPage);
     return routeWebPageCtrl.comp;
 };
