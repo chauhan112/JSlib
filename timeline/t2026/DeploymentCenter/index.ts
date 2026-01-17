2ad9b8443ddc8cbea6432a7d4dd4df7df14d070e
Raja <rajababuchauhan500@gmail.com>
2026-01-17 11:58:36 +0100

    code refactored and created separate page for each

--- timeline/t2026/DeploymentCenter/index.ts
+++ timeline/t2026/DeploymentCenter/index.ts
@@ -1,113 +1,78 @@
 import { Tools } from "../../t2025/april/tools";
-import {SidebarCtrl, RouteWebPage, DefaultPageContent} from "../../t2025/dec/DomainOpsFrontend/route/ui";
-import { HeaderWithSearch } from "./Components";
-import { MainCtrl as ListDisplayerMainCtrl, ListDisplayerCtrl } from "../../t2025/dec/DomainOpsFrontend/components/ListDisplayer";
-import { MainCtrl as RouteWebPageMainCtrl } from "../../t2025/dec/DomainOpsFrontend/route/controller";
-import { MainRouteController, type IRouteController } from "./routeController";
-
-
+import { DefaultPageContent } from "../../t2025/dec/DomainOpsFrontend/route/ui";
+import { type IApp, type IRouteController } from "./routeController";
+import { HomeRouteController, MainCtrl as DefaultPageSkeletonMainCtrl } from "./apps/defaults";
+import { SettingsPageCtrl } from "./settings";
+import { GitRepoPageCtrl } from "./apps/git-cloner";
 export const DeploymentCenterPage = () => {
-    const header = HeaderWithSearch();
-    const bodyContent = Tools.comp("div", {
-        class: "flex-1 overflow-x-hidden overflow-y-auto bg-gray-100 p-6 lg:p-8",
+    return Tools.comp("div", {
+        class: "flex-1 flex flex-col",
         textContent: "Hello World",
     });
-    return Tools.comp("div", {
-        class: "flex-1 flex flex-col h-screen overflow-hidden",
-        children: [header, bodyContent],
-    }, {}, { header, bodyContent });
 };
-
-export class WebPageWithNav{
-    comp: any;
-    sidebar_ctrl: SidebarCtrl = new SidebarCtrl();
-    setup() {
-        this.sidebar_ctrl.set_comp(this.comp.s.sidebar);
-        this.sidebar_ctrl.setup();
-        this.comp.s.header.s.hamburger_btn.update({}, { click: () => this.toggle_sidebar() });
-        this.comp.s.sidebar.s.header.s.close_btn.update({}, { click: () => this.toggle_sidebar() });
-        this.comp.s.header.s.title.update({}, { click: () => globalThis.location.href = "/" });
-        this.sidebar_ctrl.get_menu_item = this.get_menu_item;
-    }
-    set_comp(comp: any) {
-        this.comp = comp;
-    }
-    toggle_sidebar() {
-        this.comp.s.sidebar.getElement().classList.toggle('-translate-x-full');
-        this.comp.s.overlay.getElement().classList.toggle('hidden');
-    }
-    set_app_name(app_name: string) {
-        this.comp.s.header.s.title.update({textContent: app_name});
-        this.sidebar_ctrl.set_app_name(app_name);
-    }
-    get_menu_item(label: string, href: string) {
-        return Tools.comp("a", {
-            href: "#" + href,
-            class: "nav-link flex items-center px-4 py-3 rounded-md transition-colors duration-200 hover:bg-gray-700",
-            textContent: label,
-        })
-    }
-}
-
-export class HeaderController {
-    comp: any;
-    setup() {
-        this.comp.s.title.update({}, { click: () => globalThis.location.href = "/"  });
-        this.comp.s.setting.update({}, { click: () => RouteWebPageMainCtrl.navigate("/settings") });
-    }
-    set_comp(comp: any) {
-        this.comp = comp;
-    }
-}
 
 export class DeploymentCenterPageCtrl {
     comp: any;
     apps: {name: string, href: string}[] = [];
-    app_list_ctrl: ListDisplayerCtrl | undefined;
-    header_ctrl: HeaderController = new HeaderController();
     routes: IRouteController[] = [];
+    home_route_ctrl: HomeRouteController = new HomeRouteController();
+    settings_route_ctrl: SettingsPageCtrl = new SettingsPageCtrl();
+
     constructor() {
         globalThis.addEventListener("hashchange", () => this.route());
     }
     setup() {
-        this.header_ctrl.set_comp(this.comp.s.header);
-        this.header_ctrl.setup();
-        this.app_list_ctrl = ListDisplayerMainCtrl.listDisplayer(this.apps, 10, (data: any) => {
-            RouteWebPageMainCtrl.navigate(data.href);
-        }, undefined, []);
-        this.app_list_ctrl.title_getter = (data: any) => data.name;
-        this.routes.push(MainRouteController.route("", (params: any) => {
-            this.app_list_ctrl!.update();
-            return Tools.comp("div", {
-                children: [this.app_list_ctrl!.comp],
-            });
-        }));
+        this.routes.push(this.home_route_ctrl);
+        this.routes.push(this.settings_route_ctrl);
         this.route();
     }
     set_comp(comp: any) {
         this.comp = comp;
     }
-    add_app(app: {name: string, href: string}) {
-        this.apps.push(app);
-        this.app_list_ctrl?.set_data(this.apps.map(app => ({name: app.name, href: app.href})));
+    add_app(route_ctrl: IRouteController) {
+        this.routes.push(route_ctrl);
+        let app: IApp = route_ctrl.get_info();
+        this.home_route_ctrl.add_app(app);
     }
     route() {
+        let path = globalThis.location.hash
+        if (path.startsWith("#")) {
+            path = path.slice(1);
+        }
         for (const route of this.routes) {
-            if (route.matches_path(globalThis.location.hash)) {
-                let comp = route.get_component(this);
-                this.comp.s.bodyContent.update({innerHTML: "", child: comp});
+            if (route.matches_path(path)) {
+                let comp = route.get_component({parent: this, params: this.settings_route_ctrl.get_app_infos(route.get_info())});
+                this.comp.update({innerHTML: "", child: comp});
                 return;
             }
         }
-        this.comp.s.bodyContent.update({innerHTML: "", child: DefaultPageContent()});
+        this.comp.update({innerHTML: "", child: DefaultPageContent()});
+    }
+    route_to_app(app: IApp) {
+        let path = app.href;
+        for (const route of this.routes) {
+            if (route.matches_path(path)) {
+                let comp = route.get_component({parent: this});
+                console.log(route.get_info());
+                this.comp.update({innerHTML: "", child: comp});
+                return;
+            }
+        }
     }
 }
 
 export const DeploymentCenter = () => {
     const deploymentCenterPageCtrl = new DeploymentCenterPageCtrl();
     deploymentCenterPageCtrl.set_comp(DeploymentCenterPage());
-    deploymentCenterPageCtrl.add_app({name: "App 1", href: "/app-1"});
-    deploymentCenterPageCtrl.add_app({name: "App 2", href: "/app-2"});
+    let gitRepoSearchRouteCtrl = new GitRepoPageCtrl();
+    deploymentCenterPageCtrl.add_app(gitRepoSearchRouteCtrl);
+    for (let i = 0; i < 20; i++) {
+        let app2RouteCtrl = DefaultPageSkeletonMainCtrl.defaultPageSkeleton(`/app-${i}`, {
+            name: `App ${i}`, href: `/app-${i}`, subtitle: `app ${i}`, params: []
+        });
+        deploymentCenterPageCtrl.add_app(app2RouteCtrl);
+    }
+    // deploymentCenterPageCtrl.add_app(app2RouteCtrl);
     deploymentCenterPageCtrl.setup();
     return deploymentCenterPageCtrl;
 }
@@ -118,11 +83,5 @@ export class MainCtrl {
         deploymentCenterPageCtrl.set_comp(DeploymentCenterPage());
         deploymentCenterPageCtrl.setup();
         return deploymentCenterPageCtrl;
-    }
-    static webPageWithNav() {
-        const webPageWithNavCtrl = new WebPageWithNav();
-        webPageWithNavCtrl.set_comp(RouteWebPage());
-        webPageWithNavCtrl.setup();
-        return webPageWithNavCtrl;
     }
 }
