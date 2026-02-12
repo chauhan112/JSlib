376dee4f89ec68159bda36a9de065355481c5fc0
raja <rajababuchauhan500@gmail.com>
2026-02-12 23:14:36 +0100

    adjusting route to make it more generic

--- timeline/t2026/q1/WebPageWithRoutes/generic.ts
+++ timeline/t2026/q1/WebPageWithRoutes/generic.ts
@@ -1,70 +1,78 @@
 import { GComponent } from "../../../globalComps/GComponent";
-import {RouteTool, type IPage, type IRoute, type IRouteItem } from "./interface";
-import {Tools} from "../../../globalComps/tools"
+import {
+    RouteTool,
+    type IPage,
+    type IRoute,
+    type IRouteItem,
+} from "./interface";
+import { Tools } from "../../../globalComps/tools";
 
-export class GRoute implements IRoute{
-    routes: IRouteItem[] = []
-    matched_route: IRouteItem | null = null
-    tool = new RouteTool()
+export class GRoute implements IRoute {
+    routes: IRouteItem[] = [];
+    matched_route: IRouteItem | null = null;
+    tool = new RouteTool();
     add_route(route: IRouteItem): void {
-        for( let r of this.routes) {
+        for (let r of this.routes) {
             if (r.path === route.path) {
-                throw new Error("Route already exists")
+                throw new Error("Route already exists");
             }
         }
-        this.routes.push(route)
+        this.routes.push(route);
     }
     remove_route(route: IRouteItem): void {
-        this.routes = this.routes.filter(r => r.path !== route.path)
+        this.routes = this.routes.filter((r) => r.path !== route.path);
     }
     has_route(route: IRouteItem): boolean {
-        for ( let r of this.routes) {
+        for (let r of this.routes) {
             if (r.path === route.path) {
-                this.matched_route = r
-                return true
+                this.matched_route = r;
+                return true;
             }
         }
-        return false
+        return false;
     }
     get_route_component(): GComponent | null {
         if (this.matched_route) {
-            return this.matched_route.onRouted({})
+            return this.matched_route.onRouted({});
         }
-        return null
+        return null;
     }
     clear_routes(): void {
-        this.routes = []
+        this.routes = [];
     }
     get_path_after_matched(): string {
-        return ""
+        return "";
     }
     route_to(route: string, params: any): void {
         throw new Error("Method not implemented.");
     }
 }
 
-export class WebPageWithRoutes implements IPage{
-    route = new GRoute()
-    root_comp: GComponent
+export class WebPageWithRoutes implements IPage {
+    route = new GRoute();
+    root_comp: GComponent;
     constructor() {
-        this.root_comp = Tools.comp("div",{
-            class: "flex flex-col gap-4"
-        })
+        this.root_comp = Tools.comp("div", {
+            class: "flex flex-col gap-4",
+        });
     }
-    setup(){
-        this.route.add_route({path: "/", onRouted: () => this.root_comp})
+    setup() {
+        this.route.add_route({
+            path: "/",
+            onRouted: () => this.root_comp,
+            display: () => {},
+        });
     }
-    get_component(route: string, params: any): GComponent {
-        throw new Error("Method not implemented.");
+    get_comp(route: string, params: any): GComponent {
+        return this.root_comp;
     }
     display_component(comp: GComponent): void {
         throw new Error("Method not implemented.");
     }
 }
 
-export class RouteTools{
-    static parse_route(path: string): {path: string, params: any} {
-        return {path: path, params: {}}
+export class RouteTools {
+    static parse_route(path: string): { path: string; params: any } {
+        return { path: path, params: {} };
     }
-
 }
--- timeline/t2026/q1/WebPageWithRoutes/interface.ts
+++ timeline/t2026/q1/WebPageWithRoutes/interface.ts
@@ -3,32 +3,33 @@ import { MainCtrl as RouteWebPageMainCtrl } from "../../../t2025/dec/DomainOpsFr
 
 export interface IRouteItem {
     path: string;
-    onRouted: (params: any) => GComponent
+    onRouted: (params: any) => GComponent;
+    display: (comp: GComponent) => void;
 }
 
-export interface IRouteTool{
-    route_to(route: string, params: any): void
-    route_back(n: number): void
-    relative_route(route: string, params: any): void
-    get_current_url(): string
+export interface IRouteTool {
+    route_to(route: string, params: any): void;
+    route_back(n: number): void;
+    relative_route(route: string, params: any): void;
+    get_current_url(): string;
 }
 
-export class RouteTool implements IRouteTool{
-    route_to(route: string, params: any){
+export class RouteTool implements IRouteTool {
+    route_to(route: string, params: any) {
         RouteWebPageMainCtrl.navigate(route, params);
     }
-    route_back(n: number = 1){
+    route_back(n: number = 1) {
         RouteWebPageMainCtrl.go_back(n);
     }
-    relative_route(route: string, params: any){
+    relative_route(route: string, params: any) {
         RouteWebPageMainCtrl.relative_navigate(route, params);
     }
-    get_current_url(){ 
-        return globalThis.location.hash
+    get_current_url() {
+        return globalThis.location.hash;
     }
 }
 
-export interface IRoute{
+export interface IRoute {
     tool: IRouteTool;
     matched_route: IRouteItem | null;
     add_route(route: IRouteItem): void;
@@ -39,11 +40,9 @@ export interface IRoute{
     get_path_after_matched(): string;
 }
 
-export interface IPage{
+export interface IPage {
     route: IRoute;
-    root_comp: GComponent;
-    setup(): void
-    get_component(route: string, params: any): GComponent
-    display_component(comp: GComponent): void
+    setup(): void;
+    get_comp(route: string, params: any): GComponent;
+    display_component(comp: GComponent): void;
 }
-
