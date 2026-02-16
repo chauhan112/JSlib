219fdc4be21480f4e3bc31c6fa796c9122e77d55
raja <rajababuchauhan500@gmail.com>
2026-02-16 20:48:36 +0100

    absroute and get comp func

--- timeline/t2026/q1/lister/navigators.ts
+++ timeline/t2026/q1/lister/navigators.ts
@@ -2,26 +2,30 @@ import type { GComponent } from "../../../globalComps/GComponent";
 import type { INavigator } from "./interface";
 
 export class RouteNavigator implements INavigator {
-    routes: { [key: string]: GComponent } = {};
+    routes: { [key: string]: () => GComponent } = {};
     prev_url = "";
     params: { [key: string]: any } = {};
-    add_route(route: string, comp: GComponent) {
+    add_route(route: string, comp_func: () => GComponent) {
         if (this.routes[route]) throw new Error("Route already exists");
-        this.routes[route] = comp;
+        this.routes[route] = comp_func;
     }
+
     delete_route(route: string) {
         if (!this.routes[route]) throw new Error("Route not found");
         delete this.routes[route];
     }
+
     navigate(route: string, params?: any): void {
         this.prev_url = this.get_current_url();
         let newRoute = this.get_next_route(route);
         if (params) this.params[newRoute] = params;
         globalThis.location.hash = newRoute;
     }
+
     get_current_url(): string {
         return globalThis.location.hash;
     }
+
     get_next_route(path: string) {
         let new_path = path;
         if (path.startsWith("/")) {
@@ -34,6 +38,12 @@ export class RouteNavigator implements INavigator {
             curRoute += "/" + new_path;
         }
         return curRoute;
+    }
+
+    abs_route(route: string) {
+        let nro = route;
+        if (!nro.startsWith("/")) nro = "/" + nro;
+        globalThis.location.hash = nro;
     }
     get_param_for_route(abs_route: string) {
         if (!this.params[abs_route]) return {};
