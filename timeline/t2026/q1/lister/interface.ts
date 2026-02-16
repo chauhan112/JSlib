d40bd5ba1a52da352f71337efa055cdb0626ed96
raja <rajababuchauhan500@gmail.com>
2026-02-16 11:45:26 +0100

    all interfaces required for lister

---
+++ timeline/t2026/q1/lister/interface.ts
@@ -0,0 +1,50 @@
+import type { GComponent } from "../../../globalComps/GComponent";
+import type { ISComponent } from "../../../globalComps/interface";
+
+export interface ILister {
+    set_values(data: any[]): void;
+    get_comp(): GComponent;
+}
+
+export type SearchType =
+    | "contains"
+    | "equals"
+    | "startsWith"
+    | "endsWith"
+    | "regex";
+
+export type ISearchParam = {
+    word: string;
+    type: string;
+};
+
+export interface ISearch {
+    and_search(words: ISearchParam[]): any[];
+    or_search(words: ISearchParam[]): any[];
+}
+
+export interface ISearchComponent extends ISComponent {
+    get_search_values(): ISearchParam[];
+    on_search(): void;
+}
+
+export interface IPaginator {
+    get_page(nr: number): any[];
+    get_total_pages(): number;
+}
+
+export interface IPaginatorComp extends ISComponent {
+    on_goto(nr: number): void;
+}
+
+export interface IDatamodel<T> {
+    read_all(): Promise<T[]>;
+    read(id: string): Promise<T>;
+    create(data: any): Promise<T>;
+    update(id: string | number, data: T): Promise<T>;
+    deleteIt(id: string): Promise<void>;
+}
+
+export interface INavigator {
+    navigate(route: string): void;
+}
