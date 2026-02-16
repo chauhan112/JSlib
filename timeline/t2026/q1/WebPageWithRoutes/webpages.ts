fec9841e2f98c969448d62a56e61eeff99daa8c5
raja <rajababuchauhan500@gmail.com>
2026-02-16 20:48:11 +0100

    implemented page iwith breadcrumb

--- timeline/t2026/q1/WebPageWithRoutes/webpages.ts
+++ timeline/t2026/q1/WebPageWithRoutes/webpages.ts
@@ -1,5 +1,7 @@
 import type { GComponent } from "../../../globalComps/GComponent";
 import { Tools } from "../../../globalComps/tools";
+import { Breadcrumb } from "../breadcrumb/generic";
+import type { IBreadcrumbItem } from "../breadcrumb/interface";
 import { SimpleHeader } from "./headers";
 import type { IContainer, IHeader, IWebPage } from "./interface";
 
@@ -51,6 +53,39 @@ export class SimpleWebPage implements IWebPage {
     }
 }
 
+export class WebPageWithBreadcrumb implements IWebPage {
+    comp = SimpleWebPageComp();
+    content_area = Tools.comp("div", { class: "flex-1" });
+    breadcrumb: Breadcrumb = new Breadcrumb();
+    header: IHeader = new SimpleHeader();
+
+    constructor() {
+        this.comp.s.body.update({
+            children: [this.breadcrumb.get_comp(), this.content_area],
+        });
+        this.get_header().display(this.header.get_comp());
+    }
+    get_body(): IContainer {
+        return new GenericContainer(this.content_area);
+    }
+    get_header(): IContainer {
+        return new GenericContainer(this.comp.s.header);
+    }
+    get_comp(): GComponent {
+        return this.comp;
+    }
+    set_title(title: string): void {
+        this.header.set_title(title);
+    }
+    set_breadcrumb(items: IBreadcrumbItem[]): void {
+        this.breadcrumb.set_values(items);
+    }
+    go_to(item: IBreadcrumbItem): void {
+        this.breadcrumb.values.push(item);
+        this.breadcrumb.update();
+    }
+}
+
 export class WebPageWithSideBar implements IWebPage {
     get_body(): IContainer {
         throw new Error("Method not implemented.");
@@ -62,21 +97,6 @@ export class WebPageWithSideBar implements IWebPage {
         throw new Error("Method not implemented.");
     }
     get_side_bar(): IContainer {
-        throw new Error("Method not implemented.");
-    }
-}
-
-export class WebPageWithBreadcrumb implements IWebPage {
-    get_body(): IContainer {
-        throw new Error("Method not implemented.");
-    }
-    get_header(): IContainer {
-        throw new Error("Method not implemented.");
-    }
-    get_comp(): GComponent {
-        throw new Error("Method not implemented.");
-    }
-    get_breadcrumb(): IContainer {
         throw new Error("Method not implemented.");
     }
 }
