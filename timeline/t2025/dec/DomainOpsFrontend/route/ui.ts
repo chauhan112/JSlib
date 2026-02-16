24a03ee186029faf67b4951dbb1a355f27e37d1a
raja <rajababuchauhan500@gmail.com>
2026-02-16 20:46:30 +0100

    improved the default page. now it has a backbutton

--- timeline/t2025/dec/DomainOpsFrontend/route/ui.ts
+++ timeline/t2025/dec/DomainOpsFrontend/route/ui.ts
@@ -1,27 +1,33 @@
 import { Tools } from "../../../../globalComps/tools";
-import { Menu, X } from "lucide";
+import { ArrowLeft, Menu, X } from "lucide";
 import { GComponent } from "../../../../globalComps/GComponent";
-
 
 export const Header = () => {
     const hamburger_btn = Tools.comp("button", {
         class: "block rounded-lg border border-gray-300 dark:border-gray-600 p-1.5 lg:hidden",
         children: [Tools.icon(Menu, { class: "h-6 w-6" })],
-    })
-    return Tools.comp("header", {
-        class:
-            "flex items-center justify-between px-6 py-4 bg-white shadow lg:hidden z-10",
-        children: [Tools.comp("h1", {
-            class: "text-xl font-bold text-gray-800",
-            textContent: "My App",
-            key: "title",
-        }), hamburger_btn],
-    }, {}, { hamburger_btn });
-}
+    });
+    return Tools.comp(
+        "header",
+        {
+            class: "flex items-center justify-between px-6 py-4 bg-white shadow lg:hidden z-10",
+            children: [
+                Tools.comp("h1", {
+                    class: "text-xl font-bold text-gray-800",
+                    textContent: "My App",
+                    key: "title",
+                }),
+                hamburger_btn,
+            ],
+        },
+        {},
+        { hamburger_btn },
+    );
+};
 
 export const Sidebar = () => {
     const menu_items = Tools.comp("nav", {
-        class: "flex-1 px-2 py-4 space-y-2 overflow-y-auto"
+        class: "flex-1 px-2 py-4 space-y-2 overflow-y-auto",
     });
     const footer = Tools.comp("div", {
         class: "p-4 border-t border-gray-700",
@@ -39,7 +45,7 @@ export const Sidebar = () => {
             Tools.comp("h1", {
                 class: "text-2xl font-bold select-none cursor-pointer",
                 key: "title",
-                textContent: "My App"
+                textContent: "My App",
             }),
             Tools.comp("button", {
                 class: "block rounded-lg border border-gray-300 dark:border-gray-600 p-1.5 lg:hidden",
@@ -48,16 +54,16 @@ export const Sidebar = () => {
             }),
         ],
     });
-    return Tools.comp("aside", {
-        class:
-            "absolute inset-y-0 left-0 z-30 w-64 bg-gray-800 text-white transform -translate-x-full transition duration-300 ease-in-out lg:relative lg:translate-x-0 flex flex-col shadow-lg",
-        children: [
-            header,
-            menu_items,
-            footer,
-        ],
-    }, {}, { menu_items, header, footer });
-}
+    return Tools.comp(
+        "aside",
+        {
+            class: "absolute inset-y-0 left-0 z-30 w-64 bg-gray-800 text-white transform -translate-x-full transition duration-300 ease-in-out lg:relative lg:translate-x-0 flex flex-col shadow-lg",
+            children: [header, menu_items, footer],
+        },
+        {},
+        { menu_items, header, footer },
+    );
+};
 
 export class SidebarCtrl {
     comp: any;
@@ -67,7 +73,10 @@ export class SidebarCtrl {
         this.comp = comp;
     }
     setup() {
-        this.comp.s.header.s.title.update({}, { click: () => globalThis.location.href = "/" });
+        this.comp.s.header.s.title.update(
+            {},
+            { click: () => (globalThis.location.href = "/") },
+        );
     }
     add_menu_item(label: string, href: string) {
         const menu_item = this.get_menu_item(label, href);
@@ -79,7 +88,7 @@ export class SidebarCtrl {
             href: href,
             class: "nav-link flex items-center px-4 py-3 rounded-md transition-colors duration-200 hover:bg-gray-700",
             textContent: label,
-        })
+        });
     }
     add_menu_as_component(component: GComponent, href: string) {
         this.comp.s.menu_items.update({ child: component });
@@ -87,19 +96,28 @@ export class SidebarCtrl {
     }
     select_menu_item(href: string) {
         for (const key in this.components) {
-            this.components[key].getElement().classList.remove('bg-gray-900', 'border-l-4', 'border-blue-500');
-            this.components[key].getElement().classList.add('hover:bg-gray-700');
-
+            this.components[key]
+                .getElement()
+                .classList.remove(
+                    "bg-gray-900",
+                    "border-l-4",
+                    "border-blue-500",
+                );
+            this.components[key]
+                .getElement()
+                .classList.add("hover:bg-gray-700");
         }
         if (this.components[href]) {
             const link = this.components[href].getElement();
-            link.classList.add('bg-gray-900', 'border-l-4', 'border-blue-500');
-            link.classList.remove('hover:bg-gray-700');
+            link.classList.add("bg-gray-900", "border-l-4", "border-blue-500");
+            link.classList.remove("hover:bg-gray-700");
         }
     }
     set_app_name(app_name: string) {
-        this.comp.s.header.s.title.update({textContent: app_name});
-        this.comp.s.footer.s.footer_text.update({textContent: `© ${new Date().getFullYear()} ${app_name}`});
+        this.comp.s.header.s.title.update({ textContent: app_name });
+        this.comp.s.footer.s.footer_text.update({
+            textContent: `© ${new Date().getFullYear()} ${app_name}`,
+        });
     }
 }
 
@@ -107,49 +125,59 @@ export const RouteWebPage = () => {
     const sidebar = Sidebar();
     const overlay = Tools.comp("div", {
         class: "fixed inset-0 z-20 bg-black opacity-50 transition-opacity lg:hidden hidden",
-    })
+    });
     const mainBody = Tools.comp("main", {
-        class:
-            "flex-1 overflow-x-hidden overflow-y-auto bg-gray-100 p-6 lg:p-8",
+        class: "flex-1 overflow-x-hidden overflow-y-auto bg-gray-100 p-6 lg:p-8",
         children: [DefaultPageContent()],
-    })
+    });
 
     const header = Header();
-    return Tools.comp("div", {
-        class:
-            "bg-gray-100 font-sans leading-normal tracking-normal text-gray-900",
-        children: [
-            Tools.comp("div", {
-                class: "flex h-screen overflow-hidden relative",
-                children: [
-                    overlay,
-                    sidebar,
-                    Tools.comp("div", {
-                        class: "flex-1 flex flex-col h-screen overflow-hidden",
-                        children: [
-                            header,
-                            mainBody,
-                        ],
-                    }),
-                ],
-            }),
-        ],
-    }, {}, { mainBody, sidebar, overlay, header });
-}
+    return Tools.comp(
+        "div",
+        {
+            class: "bg-gray-100 font-sans leading-normal tracking-normal text-gray-900",
+            children: [
+                Tools.comp("div", {
+                    class: "flex h-screen overflow-hidden relative",
+                    children: [
+                        overlay,
+                        sidebar,
+                        Tools.comp("div", {
+                            class: "flex-1 flex flex-col h-screen overflow-hidden",
+                            children: [header, mainBody],
+                        }),
+                    ],
+                }),
+            ],
+        },
+        {},
+        { mainBody, sidebar, overlay, header },
+    );
+};
 
-export const DefaultPageContent = (textContent: string = "Page Content") => {
+export const DefaultPageContent = (textContent: string = "Route Not Found") => {
     return Tools.comp("div", {
+        class: "flex-1 flex flex-col h-screen overflow-hidden p-4",
         children: [
+            Tools.comp(
+                "button",
+                {
+                    children: [
+                        Tools.icon(ArrowLeft, {
+                            class: "h-12 w-12 text-gray-500 cursor-pointer",
+                        }),
+                    ],
+                },
+                { click: () => history.back() },
+            ),
             Tools.comp("h2", { textContent: textContent }),
         ],
     });
-}
+};
 
 export const Page404 = () => {
     return Tools.comp("div", {
         class: "flex-1 flex flex-col h-screen overflow-hidden",
-        children: [
-            Tools.comp("h2", { textContent: "Page Not Found" }),
-        ],
+        children: [Tools.comp("h2", { textContent: "Page Not Found" })],
     });
-}
+};
