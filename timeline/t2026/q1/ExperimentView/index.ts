c5c81f4099af994f324cb08f8703abd079e44a5d
raja <rajababuchauhan500@gmail.com>
2026-02-16 14:48:21 +0100

    code moved to try new approach because old approach was not scaling

--- timeline/t2026/q1/ExperimentView/index.ts
+++ timeline/t2026/q1/ExperimentView/index.ts
@@ -1,103 +1,81 @@
-import { CrudList } from "../../../t2026/DeploymentCenter/apps/domOps/crud_list";
-import type {
-    IRouteController,
-    IApp,
-} from "../../../t2026/DeploymentCenter/interfaces";
 import type { GComponent } from "../../../globalComps/GComponent";
-import type { ITableCrud } from "./interface";
-import { TableCrud } from "./generic";
-import type { IPage, IRoute } from "../WebPageWithRoutes/interface";
-import { GRoute } from "../WebPageWithRoutes/generic";
+import type { ISComponent } from "../../../globalComps/interface";
+import { Tools } from "../../../globalComps/tools";
+import {
+    type IApp,
+    type IRouteController,
+    GRouterController,
+} from "../../DeploymentCenter/interfaces";
+import {
+    DirectusTableModel,
+    TokenFromLocalStorage,
+} from "../lister/data_model";
+import type { IDatamodel, ILister, INavigator } from "../lister/interface";
+import { Lister } from "../lister/listers";
+import { RouteNavigator } from "../lister/navigators";
 
-export class DirectusTableCrud implements IRouteController, IPage {
-    route: IRoute = new GRoute();
-    crud = new CrudList();
-    initialized: boolean = false;
-    infos: IApp = {
-        name: "TableCrud",
-        href: "/table-crud",
-        subtitle: "crud table",
-        params: ["directus-url", "directus-token"],
-    };
-    inp: ITableCrud = new TableCrud(this.crud);
-    fetched: boolean = false;
-    private selected_router: IRoute | CrudList | null = null;
-    setup() {
-        this.crud.base_path = "";
-        this.crud.model.model.read_all = this.inp.read_all.bind(this.inp);
-        this.crud.model.searchCtrl.search.data.get_data =
-            this.inp.read_all_for_search.bind(this.inp);
-        this.crud.model.model.read = this.inp.read.bind(this.inp);
-        this.crud.model.searchCtrl.search.active_comp.create =
-            this.inp.create_on;
-        this.crud.model.searchCtrl.search.active_comp.filter =
-            this.inp.filter_on;
-        this.crud.model.contextMenuOptions.get_options = () =>
-            this.inp.contextMenus;
-        this.crud.model.contextMenuOptions.clicked =
-            this.inp.clicked.bind(this);
-        this.crud.model.createFormFields.fields = this.inp.create_fields;
-        this.crud.model.updateFormFields.fields = this.inp.update_fields;
-        this.crud.model.model.create = this.inp.create.bind(this.inp);
-        this.crud.model.model.update = this.inp.update.bind(this.inp);
-        this.crud.model.model.deleteIt = this.inp.deleteIt.bind(this.inp);
-        this.crud.model.get_page_size = () => this.inp.page_size;
-        this.crud.setup();
-        // this.route.add_route({
-        //     path: "/",
-        //     onRouted: () => this.crud.comp,
-        //     display: () => {},
-        //     root_comp: this.crud.comp,
-        // });
-        // this.route.add_route({
-        //     path: "",
-        //     onRouted: () => this.crud.comp,
-        //     display: () => {},
-        //     root_comp: this.crud.comp,
-        // });
-        this.initialized = true;
-    }
-    matches_path(path: string): boolean {
-        let after = path.slice(this.infos.href.length).trim();
-        if (!this.initialized) this.setup();
-        if (path.startsWith(this.infos.href)) {
-            if (this.route.has_route(after)) {
-                this.selected_router = this.route;
-                return true;
-            } else if (this.crud.matches_path(after)) {
-                this.selected_router = this.crud;
-                return true;
-            }
-        }
-        return false;
-    }
+const SingleExpView = () => {
+    return Tools.comp("div", { textContent: "Single Exp View" });
+};
 
-    get_component(params: any): GComponent {
-        if (!this.fetched) {
-            let url = params["directus-url"];
-            let token = params["directus-token"];
-            if (url && url.endsWith("/")) url = url.slice(0, -1);
-            this.inp.model.set_url_and_token(url, token);
-            this.crud.fetch_data_and_update();
-            this.fetched = true;
-        }
-        if (this.selected_router) {
-            if (this.selected_router === this.crud)
-                return this.crud.get_component(params);
-            return (this.selected_router as IRoute).get_route_component();
-        }
+export class ExpView implements ISComponent {
+    lister: ILister = new Lister();
+    model: IDatamodel<any>;
+    navigator: INavigator = new RouteNavigator();
+    private cur_path: string = "";
+    comp: GComponent | null = null;
+    constructor() {
+        this.model = new DirectusTableModel(
+            "raja_tasks",
+            new TokenFromLocalStorage("DeploymentCenterSettings"),
+        );
+        (this.model as DirectusTableModel).columns = ["id", "title"];
+
+        (this.lister as Lister).on_click = (data: any) =>
+            this.navigator.navigate("/exp", data);
+        this.define_routes();
+    }
+    define_routes() {
+        let nav = this.navigator as RouteNavigator;
+        nav.add_route("/exp", SingleExpView());
+        nav.add_route("", this._root_path());
+    }
+    private _root_path() {
+        if (this.comp) return this.comp;
+        this.comp = this.lister.get_comp();
+        this.model.read_all().then((data: any) => this.lister.set_values(data));
+        return this.comp;
+    }
+    get_comp(): GComponent {
+        let nav = this.navigator as RouteNavigator;
+        let path_to = this.cur_path.trim();
+        if (nav.routes[path_to]) return nav.routes[path_to];
         throw new Error("Route not found");
     }
-    set_info(info: IApp): void {
-        this.infos = info;
+    matches_path(path: string): boolean {
+        this.cur_path = path;
+        return (this.navigator as RouteNavigator).routes[path] !== undefined;
     }
-    get_info(): IApp {
-        return this.infos;
+}
+
+export class ExpViewPage extends GRouterController {
+    info: IApp = {
+        name: "Exp-View",
+        href: "/exp-view",
+        subtitle: "new-interface-way",
+        params: [],
+    };
+    exp_view: ExpView = new ExpView();
+    initialized: boolean = true;
+
+    get_component(params: any): GComponent {
+        return this.exp_view.get_comp();
     }
-    get_comp(route: string, params: any): GComponent {
-        return this.get_component(params);
-    }
-    display_component(comp: GComponent): void {
-        throw new Error("Method not implemented.");
+    matches_path(path: string): boolean {
+        let remPath = path.slice(this.info.href.length).trim();
+        return (
+            path.startsWith(this.info.href) &&
+            this.exp_view.matches_path(remPath)
+        );
     }
 }
