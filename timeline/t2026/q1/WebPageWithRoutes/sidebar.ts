88e88ea70daeaf0201303e87732120ab9f449d89
raja <rajababuchauhan500@gmail.com>
2026-02-16 14:50:08 +0100

    sidebar implementations skeleton

---
+++ timeline/t2026/q1/WebPageWithRoutes/sidebar.ts
@@ -0,0 +1,50 @@
+import type { GComponent } from "../../../globalComps/GComponent";
+import type { ISideNav } from "./interface";
+
+export class LeftSideBar implements ISideNav {
+    show() {
+        throw new Error("Method not implemented.");
+    }
+    hide() {
+        throw new Error("Method not implemented.");
+    }
+    get_comp(): GComponent {
+        throw new Error("Method not implemented.");
+    }
+}
+
+export class RightSideBar implements ISideNav {
+    show() {
+        throw new Error("Method not implemented.");
+    }
+    hide() {
+        throw new Error("Method not implemented.");
+    }
+    get_comp(): GComponent {
+        throw new Error("Method not implemented.");
+    }
+}
+
+export class OverlayLeftSideBar implements ISideNav {
+    show() {
+        throw new Error("Method not implemented.");
+    }
+    hide() {
+        throw new Error("Method not implemented.");
+    }
+    get_comp(): GComponent {
+        throw new Error("Method not implemented.");
+    }
+}
+
+export class OverlayRightSideBar implements ISideNav {
+    show() {
+        throw new Error("Method not implemented.");
+    }
+    hide() {
+        throw new Error("Method not implemented.");
+    }
+    get_comp(): GComponent {
+        throw new Error("Method not implemented.");
+    }
+}
