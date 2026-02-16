99660a1813eae675f7b9a5e66c2d76701f59745b
raja <rajababuchauhan500@gmail.com>
2026-02-16 14:50:39 +0100

    code refactored and new webpage tested

--- timeline/t2026/q1/WebPageWithRoutes/index.ts
+++ timeline/t2026/q1/WebPageWithRoutes/index.ts
@@ -2,10 +2,18 @@ import { HeaderCtrl } from "../../DeploymentCenter/apps/domOps/webPageWithNav/He
 import { BreadcrumbCtrl } from "../breadcrumb";
 import { Tools } from "../../../globalComps/tools";
 import type { GComponent } from "../../../globalComps/GComponent";
+import {
+    GRouterController,
+    type IApp,
+} from "../../DeploymentCenter/interfaces";
+import { SimpleWebPage } from "./webpages";
+import { FormMainCtrl } from "../dynamicFormGenerator";
+import { Breadcrumb, BreadcrumbMainCtrl } from "../breadcrumb/generic";
 
 export class WebPageWithRoutesCtrl {
     headerCtrl: HeaderCtrl = new HeaderCtrl();
     breadcrumbCtrl: BreadcrumbCtrl = new BreadcrumbCtrl();
+
     constructor() {
         this.headerCtrl = new HeaderCtrl();
         this.breadcrumbCtrl = new BreadcrumbCtrl();
@@ -25,5 +33,48 @@ export class WebPageWithRoutesCtrl {
                 this.breadcrumbCtrl.input.displayer.comp,
             ],
         });
+    }
+}
+
+export class HeaderBodyOldPage extends GRouterController {
+    info: IApp = {
+        name: "Header Body Old",
+        href: "/header-body-old",
+        subtitle: "webpage with header and breadcrumbs",
+        params: [],
+    };
+    ctrl: WebPageWithRoutesCtrl = new WebPageWithRoutesCtrl();
+    initialized: boolean = false;
+    setup() {
+        this.ctrl.setup();
+    }
+    get_component(params: any): GComponent {
+        return this.ctrl.get_comp();
+    }
+}
+
+export class HeaderBodyNewPage extends GRouterController {
+    info: IApp = {
+        name: "Header Body",
+        href: "/header-body",
+        subtitle: "webpage with header and body",
+        params: [],
+    };
+    webpage: SimpleWebPage = new SimpleWebPage();
+    form = FormMainCtrl.testForm();
+    breadcrumb: Breadcrumb = new Breadcrumb();
+    initialized: boolean = true;
+    constructor() {
+        super();
+        this.breadcrumb.set_values(BreadcrumbMainCtrl.test_items());
+        this.webpage.get_body().display(
+            Tools.comp("div", {
+                class: "flex flex-col gap-4 w-full",
+                children: [this.breadcrumb.get_comp(), this.form.get_comp()],
+            }),
+        );
+    }
+    get_component(params: any): GComponent {
+        return this.webpage.get_comp();
     }
 }
