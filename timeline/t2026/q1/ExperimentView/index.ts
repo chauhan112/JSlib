39fdb8788e57c6a66372fad06fb9b78c4b3e44fc
raja <rajababuchauhan500@gmail.com>
2026-02-17 20:34:27 +0100

    code generalization and improvements

--- timeline/t2026/q1/ExperimentView/index.ts
+++ timeline/t2026/q1/ExperimentView/index.ts
@@ -1,107 +1,112 @@
 import type { GComponent } from "../../../globalComps/GComponent";
 import type { ISComponent } from "../../../globalComps/interface";
-import { Tools } from "../../../globalComps/tools";
 import {
     type IApp,
-    type IRouteController,
     GRouterController,
 } from "../../DeploymentCenter/interfaces";
-import type { IBreadcrumbItem } from "../breadcrumb/interface";
 import {
     DirectusTableModel,
     TokenFromLocalStorage,
 } from "../lister/data_model";
-import type { IDatamodel, ILister, INavigator } from "../lister/interface";
+import type { IDatamodel, ILister } from "../lister/interface";
 import { Lister } from "../lister/listers";
-import { RouteNavigator } from "../lister/navigators";
+import { SimpleRouter } from "../lister/navigators";
+import { ClickableHeader } from "../WebPageWithRoutes/headers";
 import type { IWebPage } from "../WebPageWithRoutes/interface";
-import {
-    SimpleWebPage,
-    WebPageWithBreadcrumb,
-} from "../WebPageWithRoutes/webpages";
+import { SimpleWebPage } from "../WebPageWithRoutes/webpages";
+import { LogsPage } from "./logsPage";
 
-const SingleExpView = () => {
-    return Tools.comp("div", { textContent: "Single Exp View" });
-};
-
+const ROOT_URL = "/exp-view";
+const ROOT_TITLE = "Experiment View";
 export class ExpView implements ISComponent {
     lister: ILister = new Lister();
     model: IDatamodel<any>;
-    navigator: INavigator = new RouteNavigator();
-    private cur_path: string = "";
+    private path_comp: GComponent | null = null;
     comp: GComponent | null = null;
-    page: IWebPage = new WebPageWithBreadcrumb();
+    page: IWebPage = new SimpleWebPage();
+    navigator: SimpleRouter = new SimpleRouter();
     exp_ops_list: ILister = new Lister();
     exp_ops = ["logs", "analysis", "view", "edit"];
     table_name = "raja_experiments";
+    header: ClickableHeader = new ClickableHeader();
+    logPage: LogsPage = new LogsPage();
+    private page_setup_done = false;
     constructor() {
         this.model = new DirectusTableModel(
             this.table_name,
             new TokenFromLocalStorage("DeploymentCenterSettings"),
         );
         (this.model as DirectusTableModel).columns = ["id", "title"];
-        let page = this.page as WebPageWithBreadcrumb;
+
         (this.lister as Lister).on_click = (data: any) => {
-            this.navigator.navigate("/exp", data);
-            page.go_to({
-                name: "exp-" + data.id,
-                value: data.id,
-            });
+            this.navigator.relative_route("/exp/" + data.id, data);
         };
-        page.breadcrumb.on_click = (item: any) =>
-            this.on_breadcrumb_item_click(item);
+
         this.exp_ops_list.set_values(this.exp_ops.map((op) => ({ title: op })));
-        (this.exp_ops_list as Lister).on_click = this.on_ops_clicked.bind(this);
+        (this.exp_ops_list as Lister).on_click = (item: any) => {
+            this.navigator.relative_route(item.title);
+        };
+        this.logPage.set_web_page(this.page);
+        this.logPage.set_header_setup(this.page_setup.bind(this));
         this.define_routes();
     }
 
-    private on_ops_clicked(item: any) {
-        (this.navigator as RouteNavigator).abs_route("/exp/" + item.title);
-    }
     define_routes() {
-        let nav = this.navigator as RouteNavigator;
-        nav.add_route("/exp", this.route_to_exp.bind(this));
-        nav.add_route("", this.route_to_root.bind(this));
+        this.navigator.add_path("/exp/{exp_id}", this.route_to_exp.bind(this));
+        this.navigator.add_path("", this.route_to_root.bind(this));
+        this.navigator.add_router("/exp/{exp_id}/logs", this.logPage);
     }
 
-    private route_to_exp() {
+    private route_to_exp(params: any) {
+        let cur_params = this.navigator.get_current_params();
+        if (cur_params) this.header.set_title(cur_params.title);
+        else
+            this.model.read(params.exp_id).then((data: any) => {
+                this.header.set_title(data.title);
+            });
         this.page.get_body().display(this.exp_ops_list.get_comp());
         return this._root_path();
     }
 
     private route_to_root() {
-        let page = this.page as WebPageWithBreadcrumb;
-        page.set_breadcrumb([{ name: "exps", value: "/exp-view" }]);
+        this.header.set_title(ROOT_TITLE);
         this.page.get_body().display(this.lister.get_comp());
         return this._root_path();
     }
 
-    private on_breadcrumb_item_click(item: IBreadcrumbItem) {
-        (this.navigator as RouteNavigator).abs_route(item.value as string);
-    }
-
-    private _root_path() {
-        if (this.comp) return this.comp;
-        let page = this.page as WebPageWithBreadcrumb;
-        this.comp = page.get_comp();
-        page.set_title("Experiment View");
-        (page.comp.s.body as GComponent)
+    page_setup() {
+        if (this.page_setup_done) return;
+        this.page_setup_done = true;
+        this.page.get_header().display(this.header.get_comp());
+        this.header.set_title(ROOT_TITLE);
+        this.header.on_click = () => this.navigator.abs_route(ROOT_URL);
+        this.page
+            .get_body()
+            .get_comp()
             .getElement()
             .classList.remove("max-w-7xl");
+    }
+
+    private _root_path(): GComponent {
+        if (this.comp) return this.comp;
+        this.comp = this.page.get_comp();
+        this.page_setup();
         this.model.read_all().then((data: any) => this.lister.set_values(data));
         return this.comp;
     }
 
     get_comp(): GComponent {
-        let nav = this.navigator as RouteNavigator;
-        let path_to = this.cur_path.trim();
-        if (nav.routes[path_to]) return nav.routes[path_to]();
-        throw new Error("Route not found");
+        if (this.path_comp) {
+            let comp = this.path_comp as GComponent;
+            this.path_comp = null;
+            return comp;
+        }
+        throw new Error("comp not found");
     }
 
     matches_path(path: string): boolean {
-        this.cur_path = path;
-        if ((this.navigator as RouteNavigator).routes[path]) return true;
+        this.path_comp = this.navigator.get_element(path);
+        if (this.path_comp) return true;
         return false;
     }
 }
@@ -109,7 +114,7 @@ export class ExpView implements ISComponent {
 export class ExpViewPage extends GRouterController {
     info: IApp = {
         name: "Exp-View",
-        href: "/exp-view",
+        href: ROOT_URL,
         subtitle: "new-interface-way",
         params: [],
     };
