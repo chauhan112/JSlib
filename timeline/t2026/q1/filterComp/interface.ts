9456f1ac8fc2431803c777a8e144acd2431cc283
raja <rajababuchauhan500@gmail.com>
2026-02-22 23:43:32 +0100

    filter parser for parsing and unparsing

--- timeline/t2026/q1/filterComp/interface.ts
+++ timeline/t2026/q1/filterComp/interface.ts
@@ -1,17 +1,19 @@
 import type { ISComponent } from "../../../globalComps/interface";
+import type { IDatamodel } from "../lister/interface";
 
 export type FilterItem = {
     label: string;
-    value: string;
+    value: any;
 };
 
-export interface IFilterModel {
-    read_all(): Promise<any[]>;
-    apply_filter(filter: FilterItem): Promise<any>;
-}
+export interface IFilterModel extends IDatamodel<FilterItem> {}
 
 export interface IFilterMiniComponent extends ISComponent {
     on_filter_change(filter: FilterItem): Promise<void>;
     get_selected_filter(): FilterItem;
     set_filters(filters: FilterItem[]): void;
+}
+export interface IFilterParser {
+    parse_chip_value(value: string): any;
+    unparse_chip_value(value: any): string;
 }
