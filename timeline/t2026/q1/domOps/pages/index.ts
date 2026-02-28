c890896d59fafd3389e667f54093f673eb57c709
raja <rajababuchauhan500@gmail.com>
2026-02-28 22:45:23 +0100

    added step in behavor

--- timeline/t2026/q1/domOps/pages/index.ts
+++ timeline/t2026/q1/domOps/pages/index.ts
@@ -6,12 +6,14 @@ import {
     type WebpageComponentType,
 } from "../../WebPageWithRoutes/webpage_with_nav";
 import { DevModel } from "../data_model";
-import type { Domain, IComponentPage, Operation } from "../interface";
-import { Activity, Diamond, Play, Search } from "lucide";
+import type { Activity, Domain, IComponentPage, Operation } from "../interface";
+import { Activity as ActivityIcon, Diamond, Play, Search } from "lucide";
 import { ActivityPage } from "./activityPage";
-import { AdvanceLister } from "./search_comp";
+import { AdvanceLister, UIListerWithContext } from "./search_comp";
 import type { IDatamodel } from "../../lister/interface";
 import { Factory } from "../../dynamicFormGenerator/generic";
+import { Breadcrumb } from "../../breadcrumb/generic";
+import type { IBreadcrumbItem } from "../../breadcrumb/interface";
 
 export class GenericPage<T> implements IComponentPage<T> {
     comp = Tools.div({ textContent: "Generic Page" });
@@ -43,6 +45,11 @@ export class DomainOpsPage extends GenericPage<Domain> implements ISComponent {
     get_comp(): GComponent {
         return this.lister.get_comp();
     }
+    update_ui() {
+        this.model
+            ?.read_all()
+            .then((data) => this.lister.lister!.set_values(data));
+    }
 }
 
 export class MainPage implements ISComponent {
@@ -51,6 +58,8 @@ export class MainPage implements ISComponent {
     model = new DevModel();
     domains = new DomainOpsPage();
     operations = new DomainOpsPage();
+    breadcrumbs = new Breadcrumb();
+    private body: GComponent | null = null;
     setup() {
         let comps2 = this.webpage.get_subcomponents();
         this.sidebar_setup(comps2);
@@ -58,7 +67,6 @@ export class MainPage implements ISComponent {
         this.activity.model = this.model.get_activity_model();
         this.activity.filterModel = this.model.get_filter_model();
         this.activity.set_up();
-        (comps2.sidebar.navs.get("activity") as HTMLElement).click();
         this.domains.model = this.model.get_domain_model();
         this.domains.set_up();
         this.operations.model = this.model.get_operation_model();
@@ -66,7 +74,64 @@ export class MainPage implements ISComponent {
         this.activity.get_domains = async () => this.domains.model!.read_all();
         this.activity.get_operations = async () =>
             this.operations.model!.read_all();
+        this.body = Tools.div({
+            class: "flex flex-col flex-1 gap-4",
+            children: [
+                this.breadcrumbs.get_comp(),
+                Tools.div({
+                    key: "content",
+                    class: "flex flex-col max-h-full overflow-y-auto",
+                }),
+            ],
+        });
+        this.breadcrumbs.set_values([{ name: "root", value: "" }]);
+        this.breadcrumbs.on_click = (item: IBreadcrumbItem) =>
+            this.on_breadcrumb_click(item);
+        let body = this.webpage.get_subcomponents().body;
+        body.display(this.body);
+        let lister = this.activity.activity.get_subcomponents()
+            .lister as UIListerWithContext;
+        lister.on_click = (data: Activity) => {
+            this.model.loc.push(data);
+            this.breadcrumbs.set_values([
+                { name: "root", value: "" },
+                ...this.model.loc.map((item) => ({
+                    name: item.name,
+                    value: item.id,
+                })),
+            ]);
+            console.log(this.webpage.get_subcomponents().sidebar.selected_nav);
+            let selected =
+                this.webpage.get_subcomponents().sidebar.selected_nav;
+            (comps2.sidebar.navs.get(selected) as HTMLElement).click();
+            this.activity.update_ui();
+        };
+        (comps2.sidebar.navs.get("activity") as HTMLElement).click();
     }
+
+    private on_breadcrumb_click(item: IBreadcrumbItem) {
+        this.breadcrumbs.values = this.breadcrumbs.get_till_item(item);
+
+        if (this.breadcrumbs.values.length === 1) {
+            this.model.loc = [];
+        } else {
+            let tillItems = [];
+            let last =
+                this.breadcrumbs.values[this.breadcrumbs.values.length - 1];
+            for (let item of this.model.loc) {
+                tillItems.push(item);
+                if (item.id === last.value) {
+                    break;
+                }
+            }
+            this.model.loc = tillItems;
+        }
+        this.breadcrumbs.update();
+        let selected = this.webpage.get_subcomponents().sidebar.selected_nav;
+        let comps2 = this.webpage.get_subcomponents();
+        (comps2.sidebar.navs.get(selected) as HTMLElement).click();
+    }
+
     private sidebar_setup(comps2: WebpageComponentType) {
         comps2.header_tools.set_header_clicked(() => {
             console.log("header clicked");
@@ -74,7 +139,7 @@ export class MainPage implements ISComponent {
 
         comps2.sidebar.add_nav_item({
             label: "Activity",
-            icon: Activity,
+            icon: ActivityIcon,
             value: "activity",
         });
 
@@ -99,24 +164,28 @@ export class MainPage implements ISComponent {
     get_comp(): GComponent {
         return this.webpage.get_comp();
     }
+
+    private display(comp: GComponent): void {
+        this.body?.s.content.set_props({ innerHTML: "", child: comp });
+    }
+
     private page_setup(comps2: WebpageComponentType) {
         comps2.sidebar.on_nav_item_click = (item: any) => {
-            let body = this.webpage.get_subcomponents().body;
             switch (item.value) {
                 case "activity":
-                    body.display(this.activity.get_comp());
+                    this.activity.update_ui();
+                    this.display(this.activity.get_comp());
                     break;
                 case "domains":
-                    body.display(this.domains.get_comp());
+                    this.domains.update_ui();
+                    this.display(this.domains.get_comp());
                     break;
                 case "operations":
-                    body.display(this.operations.get_comp());
+                    this.operations.update_ui();
+                    this.display(this.operations.get_comp());
                     break;
-                // case "filters":
-                //     body.display(this.home.get_comp());
-                //     break;
                 default:
-                    body.display(
+                    this.display(
                         Tools.comp("div", { textContent: "coming soon" }),
                     );
                     break;
